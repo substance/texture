e7af3cb255c1c093b020e26ae50d628beb258d2b
Michael Aufreiter <michael@substance.io>
2016-04-19 00:00:41 +0200

    ScienceWriter implementation.

--- ui/ScientistWriter.js
+++ ui/ScientistWriter.js
@@ -1,27 +1,109 @@
 'use strict';
 
-var Component = require('substance/ui/Component');
+var Controller = require('substance/ui/Controller');
+var ContainerEditor = require('substance/ui/ContainerEditor');
+var SplitPane = require('substance/ui/SplitPane');
+var ScrollPane = require('substance/ui/ScrollPane');
+var Icon = require('substance/ui/FontAwesomeIcon');
+var Toolbar = require('substance/ui/Toolbar');
+var Layout = require('substance/ui/Layout');
+var UndoTool = require('substance/ui/UndoTool');
+var RedoTool = require('substance/ui/RedoTool');
+var SwitchTextTypeTool = require('substance/packages/text/SwitchTextTypeTool');
+var StrongTool = require('substance/packages/strong/StrongTool');
+var EmphasisTool = require('substance/packages/emphasis/EmphasisTool');
+var LinkTool = require('substance/packages/link/LinkTool');
+var CodeTool = require('substance/packages/code/CodeTool');
 
-/*
-  Loads and displays a Scientist article
-
-  Renders ScientistReader on mobile and ScientistWriter on the desktop
-*/
 function ScientistWriter() {
-  Component.apply(this, arguments);
+  Controller.apply(this, arguments);
 }
 
 ScientistWriter.Prototype = function() {
 
-  // Rendering
-  // ------------------------------------
-
+  // Custom Render method for your editor
   this.render = function($$) {
-    var el = $$('div').addClass('sc-scientist-writer');
-    el.append('SCIENTIST_WRITER');
-    return el;
+    var config = this.getConfig();
+    return $$('div').addClass('sc-note-writer').append(
+      $$(SplitPane, {splitType: 'horizontal'}).append(
+        // Top area (toolbar)
+        $$('div').addClass('se-toolbar-wrapper').append(
+          $$(Layout, {width: 'large', noPadding: true}).append(
+            $$(Toolbar).append(
+              $$(Toolbar.Group).append(
+                $$(SwitchTextTypeTool, {'title': this.i18n.t('switch_text')}),
+                $$(UndoTool).append($$(Icon, {icon: 'fa-undo'})),
+                $$(RedoTool).append($$(Icon, {icon: 'fa-repeat'})),
+                $$(StrongTool).append($$(Icon, {icon: 'fa-bold'})),
+                $$(LinkTool).append($$(Icon, {icon: 'fa-bold'})),
+                $$(EmphasisTool).append($$(Icon, {icon: 'fa-italic'})),
+                $$(CodeTool).append($$(Icon, {icon: 'fa-code'}))
+              )
+            )
+          )
+        ),
+        // Bottom area (content)
+        $$(ScrollPane, {
+          scrollbarType: 'native',
+          scrollbarPosition: 'right'
+        }).append(
+          $$(Layout, {
+            width: 'large'
+          }).append(
+            $$(ContainerEditor, {
+              doc: this.props.documentSession.doc,
+              containerId: 'body',
+              name: 'bodyEditor',
+              commands: config.bodyEditor.commands,
+              textTypes: config.bodyEditor.textTypes
+            }).ref('bodyEditor')
+          )
+        ).ref('scrollableContent')
+      )
+    );
   };
 };
 
-Component.extend(ScientistWriter);
+Controller.extend(ScientistWriter);
+
+ScientistWriter.static.config = {
+  // Controller specific configuration (required!)
+  controller: {
+    // Component registry
+    components: {
+      'paragraph': require('substance/packages/paragraph/ParagraphComponent'),
+      'heading': require('substance/packages/heading/HeadingComponent'),
+      'image': require('substance/packages/image/ImageComponent'),
+      'link': require('substance/packages/link/LinkComponent'),
+      'codeblock': require('substance/packages/codeblock/CodeblockComponent'),
+      'blockquote': require('substance/packages/blockquote/BlockquoteComponent')
+    },
+    // Controller commands
+    commands: [
+      require('substance/ui/UndoCommand'),
+      require('substance/ui/RedoCommand'),
+      require('substance/ui/SaveCommand')
+    ]
+  },
+  // Custom configuration (required!)
+  bodyEditor: {
+    commands: [
+      require('substance/packages/text/SwitchTextTypeCommand'),
+      require('substance/packages/strong/StrongCommand'),
+      require('substance/packages/emphasis/EmphasisCommand'),
+      require('substance/packages/code/CodeCommand'),
+      require('substance/packages/link/LinkCommand'),
+      require('substance/packages/image/ImageCommand')
+    ],
+    textTypes: [
+      {name: 'paragraph', data: {type: 'paragraph'}},
+      {name: 'heading1',  data: {type: 'heading', level: 1}},
+      {name: 'heading2',  data: {type: 'heading', level: 2}},
+      {name: 'heading3',  data: {type: 'heading', level: 3}},
+      {name: 'codeblock', data: {type: 'codeblock'}},
+      {name: 'blockquote', data: {type: 'blockquote'}}
+    ]
+  }
+};
+
 module.exports = ScientistWriter;

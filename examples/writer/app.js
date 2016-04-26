af04c6e5f7c92a4af31b878f2b465dda560e51d2
Michael Aufreiter <michael@substance.io>
2016-04-26 11:57:56 -0400

    Provide documentUrl via configuration.

--- examples/writer/app.js
+++ examples/writer/app.js
@@ -6,7 +6,8 @@ var Component = require('substance/ui/Component');
 // Start the application
 window.onload = function() {
   // DocumentPage loads the provided document url
+  var documentUrl = document.querySelector('meta[name=documentUrl').getAttribute('content');
   window.app = Component.mount(DocumentPage, {
-    documentUrl: 'https://s3.amazonaws.com/elife-publishing-cdn/00007/elife-00007-v1.xml'
+    documentUrl: documentUrl
   }, document.body);
 };

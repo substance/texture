bdc90dfb35ea96fd408ffed9bb45ade5c0c4a365
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-06-29 16:55:41 +0200

    Added a test for <body>.

--- packages/jats/body/Body.js
+++ packages/jats/body/Body.js
@@ -12,11 +12,9 @@ Body.static.name = "body";
 
 /*
   Content
-  (
    (address | alternatives | array | boxed-text | chem-struct-wrap | code | fig | fig-group | graphic | media | preformat | supplementary-material | table-wrap | table-wrap-group | disp-formula | disp-formula-group | def-list | list | tex-math | mml:math | p | related-article | related-object | ack | disp-quote | speech | statement | verse-group | x)*,
    (sec)*,
    sig-block?
-  )
 */
 
 Body.static.defineSchema({

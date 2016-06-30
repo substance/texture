77cb828727f3327a42f752fdc89c13332bb073c2
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-06-30 16:41:14 +0200

    Added gulp lint.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var browserify = require('browserify');
 var uglify = require('gulp-uglify');
 var through2 = require('through2');
 var rename = require('gulp-rename');
+var eslint = require('gulp-eslint');
 
 var examples = ['writer'];
 
@@ -30,24 +31,34 @@ gulp.task('sass', function() {
 });
 
 gulp.task('browserify', function() {
-    examples.forEach(function(exampleFolder) {
-      gulp.src('./examples/'+exampleFolder+'/app.js')
-        .pipe(through2.obj(function (file, enc, next) {
-            browserify(file.path)
-            .bundle(function (err, res) {
-              if (err) { return next(err); }
-              file.contents = res;
-              next(null, file);
-            });
-        }))
-        .on('error', function (error) {
-            console.log(error.stack);
-            this.emit('end');
-        })
-        .pipe(uglify().on('error', function(err){console.log(err); }))
-        .pipe(gulp.dest('./dist/'+exampleFolder));
-    });
+  examples.forEach(function(exampleFolder) {
+    gulp.src('./examples/'+exampleFolder+'/app.js')
+      .pipe(through2.obj(function (file, enc, next) {
+        browserify(file.path)
+          .bundle(function (err, res) {
+            if (err) { return next(err); }
+            file.contents = res;
+            next(null, file);
+          });
+      }))
+      .on('error', function (error) {
+        console.log(error.stack);
+        this.emit('end');
+      })
+      .pipe(uglify().on('error', function(err){console.log(err); }))
+      .pipe(gulp.dest('./dist/'+exampleFolder));
+  });
+});
 
+
+gulp.task('lint', function() {
+  return gulp.src([
+    './examples/**/*.js',
+    './packages/**/*.js',
+    './test/**/*.js',
+  ]).pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('default', ['assets', 'sass', 'browserify']);

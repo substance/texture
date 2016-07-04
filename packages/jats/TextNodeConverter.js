9c307df9c2a14f3d185dfe50e5c515d8a262081f
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-07-04 22:54:04 +0200

    Added helper for testing test node converters.

--- packages/jats/TextNodeConverter.js
+++ packages/jats/TextNodeConverter.js
@@ -4,11 +4,9 @@ var extend = require('lodash/extend');
 
 module.exports = {
   import: function(el, node, converter) {
-    node.xmlAttributes = el.getAttributes();
     node.content = converter.annotatedText(el, [node.id, 'content']);
   },
   export: function(node, el, converter) {
-    el.attr(node.xmlAttributes);
     el.append(converter.annotatedText([node.id, 'content']));
   },
   // used by text node converters to reduce code redundancy

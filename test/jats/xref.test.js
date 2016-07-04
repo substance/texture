250154a3704cb68f91a1f3d158034aad3b099815
Michael Aufreiter <michael@substance.io>
2016-07-04 19:24:30 +0200

    XRef import test.

--- test/jats/xref.test.js
+++ test/jats/xref.test.js
@@ -4,6 +4,15 @@ var test = require('./test').module('jats/xref');
 
 // attributes should be preserved
 var withAttributes =
-  '<xref ref-type="bibr" rid="bib40">Price et al., 1980</xref>';
+  '<xref alt="abc" id="xref1" ref-type="bibr" rid="bib40" specific-use="abc" xml:base="test-base" xml:lang="en">Price et al., 1980</xref>';
 test.attributesConversion(withAttributes, 'xref');
 
+var simple =
+  '<xref ref-type="bibr" rid="bib40 bib41">Price et al., 1980</xref>';
+test.withFixture(simple, 'Im-/Exporting <xref>', function(t) {
+  // import
+  var importer = t.fixture.createImporter('xref');
+  var node = importer.convertElement(t.fixture.xmlElement);
+  t.deepEqual(node.targets, ['bib40', 'bib41'], 'should have targets extracted');
+  t.notNil(node.label, 'node should have a label');
+});

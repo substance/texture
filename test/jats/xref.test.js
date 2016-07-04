406a432c5c0c520cc1c8126e6b1ce04ba5e7b04f
Michael Aufreiter <michael@substance.io>
2016-07-04 17:53:39 +0200

    Start with tests for xref element.

--- test/jats/xref.test.js
+++ test/jats/xref.test.js
@@ -2,7 +2,8 @@
 
 var test = require('./test').module('jats/xref');
 
-test(' ', function(t) {
-  t.fail('Not implemented yet.');
-  t.end();
-});
+// attributes should be preserved
+var withAttributes =
+  '<xref ref-type="bibr" rid="bib40">Price et al., 1980</xref>';
+test.attributesConversion(withAttributes, 'xref');
+

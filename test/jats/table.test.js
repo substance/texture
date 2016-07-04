6dd26ed236c59183132f71b6ccb9b7e9943bcfa3
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-07-04 23:24:43 +0200

    Test for table

--- test/jats/table.test.js
+++ test/jats/table.test.js
@@ -2,7 +2,25 @@
 
 var test = require('./test').module('jats/table');
 
-test(' ', function(t) {
-  t.fail('Not implemented yet.');
+var withAttributes =
+  '<table id="mytable"'+
+  '   specific-use="test"'+
+  '   xml:base="test-base">'+
+  '</table>';
+test.attributesConversion(withAttributes, 'table');
+
+var simple =
+  '<table><tr><td></td><td></td></tr><tr><td></td><td></td></tr></table>';
+test.withFixture(simple, 'Im-/Exporting simple table', function(t) {
+  // import
+  var importer = t.fixture.createImporter('table');
+  var node = importer.convertElement(t.fixture.xmlElement);
+  t.notNil(node.htmlContent, 'node should have HTML content after import');
+  // export
+  var exporter = t.fixture.createExporter();
+  var el = exporter.convertNode(node);
+  t.ok(el.is('table'), 'exported element should be an <table>');
+  t.equal(el.findAll('tr').length, 2, '.. with 2 rows');
+  t.equal(el.findAll('td').length, 4, '.. with 4 cell');
   t.end();
 });

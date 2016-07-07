03190716546d0b53f40c72ee53945de8d084a367
Michael Aufreiter <michael@substance.io>
2016-07-07 23:30:39 +0200

    Simplify JATS editor example.

--- examples/jats-editor/app.js
+++ examples/jats-editor/app.js
@@ -9,33 +9,10 @@ var ScientistConfigurator = require('../../packages/scientist/ScientistConfigura
 var JATSEditorPackage = require('./package');
 var configurator = new ScientistConfigurator(JATSEditorPackage);
 
-function App() {
-  App.super.apply(this, arguments);
-}
-
-App.Prototype = function() {
-
-  this.getInitialState = function() {
-    return {
-      documentId: 'elife-00007',
-    };
-  };
-
-  this.render = function($$) {
-    var documentId = this.state.documentId;
-    var el = $$('div').append(
-      $$(Scientist, {
-        mode: 'publisher',
-        documentId: documentId,
-        configurator: configurator
-      })
-    );
-    return el;
-  };
-};
-
-Component.extend(App);
-
 window.onload = function() {
-  window.app = App.static.mount(document.body);
+  window.app = Scientist.static.mount({
+    mode: 'publisher',
+    documentId: 'elife-00007',
+    configurator: configurator
+  }, document.body);
 };

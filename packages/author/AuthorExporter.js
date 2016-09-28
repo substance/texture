20b697a996dd6263b0f20efb8223b84464278e73
Daniel Beilinson <beilinson@gmail.com>
2016-09-29 00:28:39 +0300

    ES6 Exporters.

--- packages/author/AuthorExporter.js
+++ packages/author/AuthorExporter.js
@@ -1,20 +1,14 @@
 import JATSExporter from '../jats/JATSExporter'
 import JATSTransformer from './JATSTransformer'
 
-function AuthorExporter() {
-  AuthorExporter.super.apply(this, arguments);
+class AuthorExporter extends JATSExporter {
+
+  exportDocument(doc) {
+    let trafo = new JATSTransformer()
+    doc = trafo.toJATS(doc)
+    return super.exportDocument(doc)
+  }
+
 }
 
-AuthorExporter.Prototype = function() {
-  var _super = AuthorExporter.super.prototype;
-
-  this.exportDocument = function(doc) {
-    var trafo = new JATSTransformer();
-    doc = trafo.toJATS(doc);
-    return _super.exportDocument.call(this, doc);
-  };
-};
-
-JATSExporter.extend(AuthorExporter);
-
-export default AuthorExporter;
+export default AuthorExporter

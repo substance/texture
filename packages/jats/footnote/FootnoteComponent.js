7042dedc6ddc90751d96772ae5463ecf2e58cc78
Daniel Beilinson <beilinson@gmail.com>
2016-09-29 13:53:57 +0300

    ES6 jats footnote package.

--- packages/jats/footnote/FootnoteComponent.js
+++ packages/jats/footnote/FootnoteComponent.js
@@ -1,43 +1,35 @@
-'use strict';
-
 import { Component, TextPropertyComponent } from 'substance'
 import renderNodeComponent from '../../../util/renderNodeComponent'
 
-function FootnoteComponent() {
-  Component.apply(this, arguments);
-}
+class FootnoteComponent extends Component {
 
-FootnoteComponent.Prototype = function() {
+  render($$) {
+    let node = this.props.node
+    let doc = node.getDocument()
 
-  this.render = function($$) {
-    var node = this.props.node;
-    var doc = node.getDocument();
-
-    var el = $$('div')
+    let el = $$('div')
       .addClass('sc-footnote')
-      .attr('data-id', this.props.node.id);
+      .attr('data-id', this.props.node.id)
 
     if (node.label) {
-      var label = doc.get(node.label);
+      let label = doc.get(node.label)
       el.append($$(TextPropertyComponent, {
         path: label.getTextPath()
-      }));
+      }))
     }
     // TODO: what if no label is present?
 
     this.props.node.nodes.forEach(function(nodeId) {
-      var childNode = doc.get(nodeId);
+      let childNode = doc.get(nodeId)
       el.append(
         renderNodeComponent(this, $$, childNode, {
           disabled: this.props.disabled
         })
       );
-    }.bind(this));
+    }.bind(this))
 
-    return el;
-  };
-};
+    return el
+  }
+}
 
-Component.extend(FootnoteComponent);
-
-export default FootnoteComponent;
+export default FootnoteComponent

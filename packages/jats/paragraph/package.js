477ddaa9a78045733371e2bb730a6ff933a4fdc4
Daniel Beilinson <beilinson@gmail.com>
2016-09-29 14:31:50 +0300

    ES6 jats paragraph package.

--- packages/jats/paragraph/package.js
+++ packages/jats/paragraph/package.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import Paragraph from './Paragraph'
 import ParagraphComponent from './ParagraphComponent'
 import ParagraphConverter from './ParagraphConverter'
@@ -9,20 +7,20 @@ export default {
   name: 'paragraph',
 
   configure: function(config) {
-    config.addNode(Paragraph);
-    config.addComponent(Paragraph.type, ParagraphComponent);
-    config.addConverter('jats', ParagraphConverter);
+    config.addNode(Paragraph)
+    config.addComponent(Paragraph.type, ParagraphComponent)
+    config.addConverter('jats', ParagraphConverter)
     config.addTextType({
       name: Paragraph.type,
       data: {type: Paragraph.type}
-    });
+    })
     config.addLabel(Paragraph.type, {
       en: 'Paragraph',
       de: 'Paragraph'
-    });
+    })
     config.addLabel('paragraph.content', {
       en: 'Paragraph',
       de: 'Paragraph'
-    });
+    })
   }
-};
+}

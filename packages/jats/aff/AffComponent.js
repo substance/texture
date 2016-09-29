802fbae45892075dc46a88bea89179ca7a2b21c2
Daniel Beilinson <beilinson@gmail.com>
2016-09-29 11:06:03 +0300

    ES6 Aff package.

--- packages/jats/aff/AffComponent.js
+++ packages/jats/aff/AffComponent.js
@@ -1,18 +1,16 @@
-'use strict';
-
 import { Component, FontAwesomeIcon as Icon } from 'substance'
 import { getAdapter } from './affUtils'
 
 class AffComponent extends Component {
   constructor(...args) {
-    super(...args);
+    super(...args)
   }
 
   render($$) {
     var aff = getAdapter(this.props.node);
     var el = $$('div').addClass('sc-aff')
       .append($$(Icon, {icon: 'fa-building-o'}))
-      .append(' '+aff.name);
+      .append(' '+aff.name)
     return el;
   }
 }
--- packages/jats/aff/affUtils.js
+++ packages/jats/aff/affUtils.js
@@ -4,10 +4,10 @@ import toDOM from '../../../util/toDOM'
   Get all affiliations for a doc
 */
 function getAffs(doc) {
-  var affs = doc.getIndex('type').get('aff');
+  var affs = doc.getIndex('type').get('aff')
   // Convert to array and get the view for the node
-  affs = Object.keys(affs).map(affId => getAdapter(affs[affId]));
-  return affs;
+  affs = Object.keys(affs).map(affId => getAdapter(affs[affId]))
+  return affs
 }
 
 /*

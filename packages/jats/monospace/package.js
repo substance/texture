3f6b9ab0e18cb6696f3f9fba856d011d62e2b890
Daniel Beilinson <beilinson@gmail.com>
2016-09-29 14:31:38 +0300

    ES6 jats monospace package.

--- packages/jats/monospace/package.js
+++ packages/jats/monospace/package.js
@@ -1,12 +1,10 @@
-'use strict';
-
 import Monospace from './Monospace'
 import MonospaceConverter from './MonospaceConverter'
 
 export default {
   name: 'monospace',
   configure: function(config) {
-    config.addNode(Monospace);
-    config.addConverter('jats', MonospaceConverter);
+    config.addNode(Monospace)
+    config.addConverter('jats', MonospaceConverter)
   }
-};
+}

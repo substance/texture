f07b910b9aff1b5e747282f8c1a70b8cf215f9ce
Daniel Beilinson <beilinson@gmail.com>
2016-09-29 13:57:18 +0300

    ES6 jats graphic package.

--- packages/jats/graphic/GraphicComponent.js
+++ packages/jats/graphic/GraphicComponent.js
@@ -1,27 +1,19 @@
-'use strict';
-
 import { Component } from 'substance'
 
-function GraphicComponent() {
-  Component.apply(this, arguments);
-}
+class GraphicComponent extends Component {
 
-GraphicComponent.Prototype = function() {
-
-  this.render = function($$) {
-    var node = this.props.node;
-    var el = $$('div')
+  render($$) {
+    let node = this.props.node
+    let el = $$('div')
       .addClass('sc-graphic')
-      .attr('data-id', node.id);
+      .attr('data-id', node.id)
     el.append(
       $$('img').attr({
         src: node.getHref()
       })
-    );
-    return el;
-  };
-};
+    )
+    return el
+  }
+}
 
-Component.extend(GraphicComponent);
-
-export default GraphicComponent;
+export default GraphicComponent

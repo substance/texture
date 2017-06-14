53b331517d26db740c4f76125a5ed9ddc2b9b42d
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-06-14 02:04:23 +0200

    Unsupported as IsolatedNode

--- src/editor/components/UnsupportedNodeComponent.js
+++ src/editor/components/UnsupportedNodeComponent.js
@@ -1,11 +1,18 @@
-import { Component } from 'substance'
+import { Component, IsolatedNodeComponent } from 'substance'
 
-export default class UnsupportedNodeComponent extends Component {
+export default class UnsupportedNodeComponent extends IsolatedNodeComponent {
+  _getContentClass() {
+    return UnsupportedContentComponent
+  }
+}
+
+class UnsupportedContentComponent extends Component {
 
   render($$) {
     const node = this.props.node
     let el = $$('div').addClass('sc-unsupported')
       .attr('data-id', node.id)
+      .attr('contenteditable', false)
     el.append(
       $$('pre').text(node.toXML().serialize())
     )

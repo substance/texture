132db7e7958f32f21bc52314830875ded0a25e20
Michael Aufreiter <michael@substance.io>
2017-06-14 10:42:02 +0200

    Make Metadata component scrollable.

--- src/editor/components/MetadataComponent.js
+++ src/editor/components/MetadataComponent.js
@@ -3,21 +3,24 @@ import { Component } from 'substance'
 export default class MetadataComponent extends Component {
 
   render($$) {
+    const ScrollPane = this.getComponent('scroll-pane')
     const doc = this.context.editorSession.getDocument()
     const articleMeta = doc.find('article-meta')
-
     let el = $$('div').addClass('sc-metadata')
+
+    let scrollPane = $$(ScrollPane).ref('metadataScroll')
 
     const contribGroup = articleMeta.findChild('contrib-group')
     if (contribGroup) {
-      el.append($$(this.getComponent('contributors'), { node: contribGroup }))
+      scrollPane.append($$(this.getComponent('contributors'), { node: contribGroup }))
     }
 
     const affGroup = articleMeta.findChild('aff-group')
     if (affGroup) {
-      el.append($$(this.getComponent('affiliations'), { node: affGroup }))
+      scrollPane.append($$(this.getComponent('affiliations'), { node: affGroup }))
     }
 
+    el.append(scrollPane)
     return el
   }
 }

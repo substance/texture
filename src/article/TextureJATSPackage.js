1cc766218a05ba14b0f1801d734ad891c3442798
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-06-14 13:27:13 +0200

    First iteration on supporting cross application pasting.

--- src/article/TextureJATSPackage.js
+++ src/article/TextureJATSPackage.js
@@ -2,11 +2,16 @@ import { registerSchema } from 'substance'
 import TextureDocument from './TextureDocument'
 import TextureJATS from './TextureJATS'
 import TextureJATSImporter from './TextureJATSImporter'
+import TextureHTMLConverters from './TextureHTMLConverters'
 
 export default {
   name: 'TextureJATS',
   configure(config) {
     registerSchema(config, TextureJATS, TextureDocument)
     config.addImporter(TextureJATS.getName(), TextureJATSImporter)
+    // enable rich-text support for clipboard
+    TextureHTMLConverters.forEach((converter) => {
+      config.addConverter('html', converter)
+    })
   }
 }

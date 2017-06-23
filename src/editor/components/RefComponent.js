e90cf96856d279a7730324981563885dff5a8cf8
Daniel Beilinson <beilinson@gmail.com>
2017-06-23 15:32:11 +0300

    Additional styles for fn-group and ref-list.

--- src/editor/components/RefComponent.js
+++ src/editor/components/RefComponent.js
@@ -9,22 +9,26 @@ export default class RefComponent extends Component {
 
     let stringCitation = ref.find('string-citation')
     if (stringCitation) {
-      el.append(
-        $$('div').addClass('sc-string-citation').append(
+      let stringCitationEl = $$('div').addClass('sc-string-citation')
+      if(label) {
+        stringCitationEl.append(
           $$('div').addClass('se-label').append(
             label
-          ),
-          $$(TextPropertyEditor, {
-            path: stringCitation.getTextPath(),
-            disabled: this.props.disabled
-          }),
-          $$('div').addClass('se-remove-ref')
-            .append(
-              $$(Icon, { icon: 'fa-trash' })
-            )
-            .on('click', this._removeRef.bind(this, ref.id))
+          )
         )
+      }
+      stringCitationEl.append(
+        $$(TextPropertyEditor, {
+          path: stringCitation.getTextPath(),
+          disabled: this.props.disabled
+        }),
+        $$('div').addClass('se-remove-ref')
+          .append(
+            $$(Icon, { icon: 'fa-trash' })
+          )
+          .on('click', this._removeRef.bind(this, ref.id))
       )
+      el.append(stringCitationEl)
     } else {
       console.warn('No string citation found')
     }

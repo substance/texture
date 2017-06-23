f44eb22b52d14d23eff848406a7b18ac14c355be
Daniel Beilinson <beilinson@gmail.com>
2017-06-23 20:40:03 +0300

    Remove unneeded render methods.

--- src/editor/components/TableHeadComponent.js
+++ src/editor/components/TableHeadComponent.js
@@ -1,19 +1,3 @@
 import ElementNodeComponent from './ElementNodeComponent'
 
-export default class TableHeadComponent extends ElementNodeComponent {
-
-  render($$) {
-    let node = this.props.node
-    let el = $$('thead')
-    node.childNodes.forEach(child => {
-      let doc = this.context.doc
-      let childNode = doc.get(child)
-      let comp = this.getComponent(childNode.type)
-      if(comp) {
-        el.append($$(comp, {node: childNode}))
-      }
-    })
-
-    return el
-  }
-}
+export default class TableHeadComponent extends ElementNodeComponent {}

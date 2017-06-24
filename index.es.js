6552951a2d42d934d928970945569fb1ca35b78e
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-06-24 19:51:10 +0200

    Added index files for article and converter

--- index.es.js
+++ index.es.js
@@ -1,4 +1,6 @@
-export { default as TextureJATSPackage } from './src/article/TextureJATSPackage'
-export { default as jats2texture } from './src/article/jats2texture'
+// TODO: add index files for the other folders as well
+export * from './src/article/index'
+export * from './src/converter/index'
+
 export { default as EditorPackage } from './src/editor/EditorPackage'
 export { default as TextureConfigurator } from './src/editor/util/TextureConfigurator'

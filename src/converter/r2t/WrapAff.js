ddea0dec9cce2bf71b004759216949c933afe624
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-07-21 19:48:21 +0200

    Added converters.

--- src/converter/r2t/WrapAff.js
+++ src/converter/r2t/WrapAff.js
@@ -5,10 +5,10 @@ export default class WrapAff {
 
   import(dom) {
     let articleMeta = dom.find('article-meta')
-    let allAffs = dom.findAll('aff')
+    let allAffs = dom.findAll('article-meta > aff')
     let affGroup = dom.createElement('aff-group')
     affGroup.append(allAffs)
-
+    // now insert the aff-group at the correct position
     let articleMetaSchema = TextureJATS.getElementSchema('article-meta')
     let pos = articleMetaSchema.findFirstValidPos(articleMeta, 'aff-group')
     articleMeta.insertAt(pos, articleMeta)

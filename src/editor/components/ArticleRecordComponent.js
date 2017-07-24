773586ed6dc122c05efaed8e56efd50c3a3110bb
Michael Aufreiter <michael@substance.io>
2017-07-24 14:55:36 +0200

    Overhaul ArticleRecord editor.

--- src/editor/components/ArticleRecordComponent.js
+++ src/editor/components/ArticleRecordComponent.js
@@ -1,50 +1,85 @@
 import { NodeComponent } from 'substance'
 
-/*
-  Edit article record in this MetadataSection
-*/
+const LABELS = {
+  'print': 'Print',
+  'electronic': 'Electronic'
+}
+
 export default class ArticleRecordComponent extends NodeComponent {
 
+
+  didMount() {
+    super.didMount()
+    let articleMeta = this.props.node
+    let contentLoc = articleMeta.find('content-loc')
+    this.context.editorSession.onRender('document', this.rerender, this, { path: [contentLoc.id]})
+  }
+
+  dispose() {
+    super.dispose()
+  }
+
   render($$) {
-    let el = $$('div').addClass('sc-article-info')
+    let el = $$('div').addClass('sc-article-record')
 
     let articleMeta = this.props.node
+    let contentLoc = articleMeta.find('content-loc')
+    let contentLocType = contentLoc.attr('type')
 
-    // TODO: if not present we need either a way to
-    // add these or create and prune on Im-/Export
-    let pubDate = articleMeta.findChild('pub-date')
     let volume = articleMeta.findChild('volume')
     let issue = articleMeta.findChild('issue')
-    let fpage = articleMeta.findChild('fpage')
-    let lpage = articleMeta.findChild('lpage')
+    let fpage = contentLoc.findChild('fpage')
+    let lpage = contentLoc.findChild('lpage')
+    let pageRange = contentLoc.findChild('page-range')
+    let eLocationId = contentLoc.findChild('elocation-id')
 
+    el.append(this._renderContentLocTypeSwitcher($$, contentLocType))
 
-    if (pubDate) {
-      el.append(
-        this._renderDateEditor($$, pubDate, 'Publication Date')
-      )
-    }
-    if (volume) {
-      el.append(
-        this._renderTextEditor($$, volume, 'Volume', 'number')
-      )
-    }
-    if (issue) {
-      el.append(
-        this._renderTextEditor($$, issue, 'Issue' ,'text')
-      )
-    }
-    if (fpage) {
-      el.append(
-        this._renderTextEditor($$, fpage, 'First Page', 'number')
-      )
-    }
-    if (lpage) {
-      el.append(
-        this._renderTextEditor($$, lpage, 'Last Page', 'number')
-      )
+    if (contentLocType === 'print') {
+      // TODO: we should make these non-optional in TextureJATS
+      if (volume) {
+        el.append(this._renderTextEditor($$, volume, 'Volume', 'text'))
+      }
+      if (issue) {
+        el.append(this._renderTextEditor($$, issue, 'Issue', 'text'))
+      }
+      el.append(this._renderTextEditor($$, fpage, 'First page', 'text'))
+      el.append(this._renderTextEditor($$, lpage, 'Last page', 'text'))
+      el.append(this._renderTextEditor($$, pageRange, 'Page Range', 'text'))
+    } else if (contentLocType === 'electronic') {
+      el.append(this._renderTextEditor($$, eLocationId, 'E-Location ID', 'text'))
     }
     return el
+  }
+
+  _renderContentLocTypeSwitcher($$, contentLocType) {
+    let el = $$('div').addClass('se-content-loc-type')
+    let select = $$('select').on('change', this._onContentLocTypeChange).ref('contentLocType')
+    let options = ['print', 'electronic']
+
+    options.forEach((option) => {
+      let optionEl = $$('option').attr('value', option).append(
+        LABELS[option]
+      )
+      if (option === contentLocType) {
+        optionEl.attr('selected', 'selected')
+      }
+      select.append(optionEl)
+    })
+
+    el.append(
+      $$('div').addClass('se-label').append('Type'),
+      $$('div').addClass('se-select').append(select)
+    )
+    return el
+  }
+
+  _onContentLocTypeChange() {
+    let newContentLocType = this.refs.contentLocType.val()
+    this.context.editorSession.transaction((tx) => {
+      let contentLoc = tx.find('article-meta > content-loc')
+      contentLoc.attr('type', newContentLocType)
+    })
   }
 
   _renderTextEditor($$, metaEl, name, type) {
@@ -57,21 +92,6 @@ export default class ArticleRecordComponent extends NodeComponent {
         .ref(id)
         .on('change', this._updateTextProp.bind(this, metaEl))
     )
-
-    return el
-  }
-
-  _renderDateEditor($$, metaEl, name) {
-    let id = metaEl.id
-    let value = metaEl.getAttribute('iso-8601-date')
-    let el = $$('div').addClass('se-metadata-item').append(
-      $$('div').addClass('se-label').append(name),
-      $$('input').attr({type: 'date', value: value})
-        .addClass('se-text-input')
-        .ref(id)
-        .on('change', this._updateDateProp.bind(this, metaEl))
-    )
-
     return el
   }
 
@@ -83,12 +103,6 @@ export default class ArticleRecordComponent extends NodeComponent {
     editorSession.transaction(doc => {
       doc.set(path, value)
     })
-  }
-
-  _updateDateProp(metaEl) {
-    let id = metaEl.id
-    let value = this.refs[id].val()
-    metaEl.setAttribute('iso-8601-date', value)
   }
 
 }

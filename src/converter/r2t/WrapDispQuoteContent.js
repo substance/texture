05375342d39e96c31099f6bde2b7ca8af5d5c3ed
Michael Aufreiter <michael@substance.io>
2017-08-07 17:01:05 +0200

    Strict schema + transformers for <disp-quote>.

--- src/converter/r2t/WrapDispQuoteContent.js
+++ src/converter/r2t/WrapDispQuoteContent.js
@@ -4,14 +4,26 @@ export default class WrapDispQuoteContent {
 
   import(dom) {
     dom.findAll('disp-quote').forEach((dispQuote) => {
-      let attribs = dispQuote.findAll('attrib')
+      let attrib = dispQuote.find('attrib')
+      if (!attrib) {
+        // attrib element is required in TextureJATS.
+        attrib = dom.createElement('attrib')
+      }
       let content = dom.createElement('disp-quote-content').append(dispQuote.children)
       dispQuote.append(content)
-      dispQuote.append(attribs)
+      dispQuote.append(attrib)
     })
   }
 
   export(dom) {
     dom.findAll('disp-quote-content').forEach(unwrapChildren)
+    let dispQuotes = dom.findAll('disp-quote')
+    // Remove all empty attrib elements
+    dispQuotes.forEach((dispQuote) => {
+      let attrib = dispQuote.find('attrib')
+      if (attrib.textContent === '') {
+        dispQuote.removeChild(attrib)
+      }
+    })
   }
 }

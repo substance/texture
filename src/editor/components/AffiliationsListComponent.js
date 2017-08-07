07d6a23ed5d0bce02b0032a8cd72443ff35c0f26
Nate Wright <natew@notthisway.com>
2017-08-07 14:47:29 +0100

    Add inline-editing of authors and contributors

--- src/editor/components/AffiliationsListComponent.js
+++ src/editor/components/AffiliationsListComponent.js
@@ -1,4 +1,4 @@
-import { Component } from 'substance'
+import { Component, TextPropertyEditor } from 'substance'
 
 export default class AffiliationsList extends Component {
 
@@ -15,22 +15,43 @@ export default class AffiliationsList extends Component {
   render($$) {
     const articleMeta = this.props.node
     const affiliations = articleMeta.findAll('string-aff')
-    const affs = affiliations.map(aff => { return aff.getTextContent() })
-    
-    let el = $$('div').addClass('sc-affiliations-list')
-      .append(affs.join('; '))
+    let els = [];
 
-    return el
+    let affiliationsEl = affiliations.map(affiliation => {
+      return $$(TextPropertyEditor, {
+        placeholder: 'Enter affiliated institution',
+        path: affiliation.getTextPath(),
+        disabled: this.props.disabled,
+        tagName: 'span'
+      }).addClass('se-affiliation')
+    })
+
+    // Separate affiliations with a semi-colon
+    // TODO this should be translatable
+    for (let affiliationEl of affiliationsEl) {
+      els.push(affiliationEl, $$('span').append(';').addClass('se-separator'))
+    }
+    els = els.slice(0, -1)
+
+    return $$('div').addClass('sc-affiliations-list')
+      .append(els)
+      .on('click', this._editAffiliations)
   }
 
   _onDocumentChange(change) {
     let updatedProps = Object.keys(change.updated)
     let updatedPropsString = updatedProps.join('/')
-    // We will trigger rerendering if any string-aff 
+    // We will trigger rerendering if any string-aff
     // or aff-group were updated
     if(updatedPropsString.indexOf('string-aff') > -1 || updatedPropsString.indexOf('aff-group') > -1) {
       this.rerender()
-    } 
+    }
+  }
+
+  _editAffiliations() {
+    this.send('switchContext', {
+      contextId: 'affiliations',
+    })
   }
 
 }

5a926e760b0e7c483549e0735afb344aa652781f
Daniel Beilinson <beilinson@gmail.com>
2017-09-01 00:46:16 +0300

    Dedicated Text Input style.

--- src/editor/components/TextInput.js
+++ src/editor/components/TextInput.js
@@ -6,13 +6,13 @@ export default class TextInput extends Component {
     let node = this.props.node
     let TextPropertyEditor = this.getComponent('text-property-editor')
 
-    return $$('div').addClass('se-source').append(
+    return $$('div').addClass('sc-text-input').append(
       $$('div').addClass('se-label').append(this.props.label),
       $$(TextPropertyEditor, {
         placeholder: this.props.label,
         path: node.getTextPath(),
         disabled: this.props.disabled
-      }).ref(node.id).addClass('se-text-input')
+      }).ref(node.id).addClass('se-text-editor')
     )
   }
 

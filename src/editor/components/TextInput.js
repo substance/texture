d922be105f0e12398a720e79d90b34c233b95b93
Daniel Beilinson <beilinson@gmail.com>
2017-09-07 16:14:06 +0300

    Make TextInput label optional.

--- src/editor/components/TextInput.js
+++ src/editor/components/TextInput.js
@@ -4,16 +4,26 @@ export default class TextInput extends Component {
 
   render($$) {
     let node = this.props.node
+    let label = this.props.label
     let TextPropertyEditor = this.getComponent('text-property-editor')
 
-    return $$('div').addClass('sc-text-input').append(
-      $$('div').addClass('se-label').append(this.props.label),
+    let el = $$('div').addClass('sc-text-input')
+
+    if(label) {
+      el.append(
+        $$('div').addClass('se-label').append(label)
+      )
+    }
+
+    el.append(
       $$(TextPropertyEditor, {
-        placeholder: this.props.label,
+        placeholder: label,
         path: node.getTextPath(),
         disabled: this.props.disabled
       }).ref(node.id).addClass('se-text-editor')
     )
+
+    return el
   }
 
 }

7de686c8ffe35ac7181fc8b6bb8ad80705f6630f
Michael Aufreiter <michael@substance.io>
2017-09-17 01:37:11 +0200

    Strictly test desired output for each input.

--- test/converter/NormalizeFn.test.js
+++ test/converter/NormalizeFn.test.js
@@ -6,33 +6,20 @@ const test = module('Footnote Normalize Transformer')
 import readFixture from '../fixture/readFixture'
 let fixture = readFixture('fn-group.xml')
 
-test("j2r: Transform paragraphs inside fn", function(t) {
+test("j2r: Unwrap fn > p > disp-quote > p", function(t) {
   let dom = DefaultDOMElement.parseXML(fixture)
   let converter = new NormalizeFn()
   converter.import(dom)
-
   let fn = dom.find('#fn3')
-  let paragraphs = fn.findAll('p')
-  t.equal(paragraphs.length, 2, 'fn should contain two paragraphs')
-  t.equal(paragraphs[0].textContent, 'paragraph 1', 'first paragraph should be "paragraph 1"')
-  t.equal(paragraphs[1].textContent, 'paragraph 2', 'second paragraph should be "paragraph 2"')
-
+  t.equal(fn.outerHTML, '<fn id="fn3"><p>disp_<bold>quote</bold></p></fn>')
   t.end()
 })
 
-test("j2r: Removing elements during fn transformation", function(t) {
+test("j2r: Removing all elements except inner p's", function(t) {
   let dom = DefaultDOMElement.parseXML(fixture)
   let converter = new NormalizeFn()
   converter.import(dom)
-
-  let fn = dom.find('#fn3')
-  t.equal(fn.children.length, 2, 'fn should contain two elements')
-  t.notOk(fn.find('fig'), 'there should be no fig element left inside fn')
-  t.notOk(fn.find('label'), 'there should be no label element left inside fn')
-  t.notOk(fn.find('caption'), 'there should be no caption element left inside fn')
-  t.notOk(fn.find('title'), 'there should be no title element left inside fn')
-  t.notOk(fn.find('graphic'), 'there should be no graphic element left inside fn')
-  t.ok(fn.find('p'), 'there should be p element left inside fn')
-
+  let fn = dom.find('#fn4')
+  t.equal(fn.outerHTML, '<fn id="fn4"><p>fig_caption</p></fn>')
   t.end()
 })

9f52951eaca47c19da0bd62c4729e6a5fe467251
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-01-17 12:44:46 +0100

    Remove obsolete code in stand-alone app.

--- app/editor.js
+++ app/editor.js
@@ -2,7 +2,6 @@ const { Texture } = window.texture
 let remote = require('electron').remote
 let args = remote.getGlobal('sharedObject').args
 
-// Loading XML: In this example we use a bundled virtual file-system
 let file = 'data/introducing-texture.xml' // without leading '/'
 
 if(args.length > 2) {
@@ -11,18 +10,5 @@ if(args.length > 2) {
 
 window.onload = function() {
   window.app = Texture.mount({
-    documentId: file,
-    /*
-      Implement your own logic to read and write XML
-    */
-    readXML: function(documentId, cb) {
-      let xmlStr = vfs.readFileSync(documentId)
-      cb(null, xmlStr)
-    },
-    writeXML: function(documentId, xml, cb) {
-      console.log('writeXML needs to be implemented for saving.')
-      console.log('XML', xml)
-      cb(null)
-    }
   }, document.body)
 }

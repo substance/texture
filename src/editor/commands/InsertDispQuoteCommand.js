147b8ed223385f5470a8c7da030dbc1b7d79bd42
Daniel <beilinson@gmail.com>
2018-01-18 17:33:37 +0300

    Fix class name.

--- src/editor/commands/InsertDispQuoteCommand.js
+++ src/editor/commands/InsertDispQuoteCommand.js
@@ -1,6 +1,6 @@
 import InsertNodeCommand from './InsertNodeCommand'
 
-export default class InsertXrefCommand extends InsertNodeCommand {
+export default class InsertDispQuoteCommand extends InsertNodeCommand {
 
   createNode(tx) {
     let dispQuote = tx.createElement('disp-quote')

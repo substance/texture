3df7b505a6c3bccc159d181995d50e1d91cf6044
Daniel <beilinson@gmail.com>
2018-01-30 22:56:54 +0300

    Towards entity creation from list.

--- src/editor/components/RefListComponent.js
+++ src/editor/components/RefListComponent.js
@@ -1,8 +1,46 @@
-import { NodeComponent, without } from 'substance'
+import { NodeComponent, isNil, without } from 'substance'
 import ModalDialog from '../../shared/ModalDialog'
 import CreateEntity from '../../entities/CreateEntity'
 import EditEntity from '../../entities/EditEntity'
 import RefComponent from './RefComponent'
+
+function prefillEntity(type, text) {
+  let defaults = {
+    type
+  }
+  if (type === 'person') {
+    let parts = text.split(' ')
+    defaults.surname = parts.pop()
+    defaults.givenNames = parts.join(' ')
+  } else if (type === 'organisation') {
+    defaults.name = text
+  } else if (type === 'book') {
+    defaults.source = text
+  } else if (type === 'journal-article') {
+    defaults.articleTitle = text
+  } else if (type === 'conference-proceeding') {
+    defaults.articleTitle = text
+  } else if (type === 'clinical-report') {
+    defaults.articleTitle = text
+  } else if (type === 'preprint') {
+    defaults.articleTitle = text
+  } else if (type === 'report') {
+    defaults.source = text
+  } else if (type === 'periodical') {
+    defaults.articleTitle = text
+  } else if (type === 'data-publication') {
+    defaults.dataTitle = text
+  } else if (type === 'patent') {
+    defaults.articleTitle = text
+  } else if (type === 'webpage') {
+    defaults.title = text
+  } else if (type === 'thesis') {
+    defaults.articleTitle = text
+  } else if (type === 'software') {
+    defaults.title = text
+  }
+  return defaults
+}
 
 export default class RefListComponent extends NodeComponent {
 
@@ -31,6 +69,7 @@ export default class RefListComponent extends NodeComponent {
     let el = $$('div').addClass('sc-ref-list')
     let bibliography = referenceManager.getBibliography()
     let mode = this.state.mode
+    let popup = this.state.popup
 
     if (mode) {
       let ModeComponent
@@ -66,31 +105,66 @@ export default class RefListComponent extends NodeComponent {
         )
       )
     }
-    el.append(
+
+    let options = $$('div').addClass('se-ref-list-options').append(
       $$('button').addClass('sc-button sm-style-big').append('Add Reference')
         .on('click', this._toggleNewReferencePopup)
     )
+
+    if(popup) {
+      options.append(
+        this._renderNewReferencePopup($$)
+      )
+    }
+
+    el.append(options)
+
     return el
   }
 
-  _renderNewReferencePopup() {
-    //      targetTypes: [
-    //         'journal-article', 'book', 'conference-proceeding',
-    //         'clinical-trial', 'preprint', 'report',
-    //         'periodical', 'data-publication', 'patent',
-    //         'webpage', 'thesis', 'software'
-    //       ]
+  _renderNewReferencePopup($$) {
+    const targetTypes = [
+      'journal-article', 'book', 'conference-proceeding',
+      'clinical-trial', 'preprint', 'report',
+      'periodical', 'data-publication', 'patent',
+      'webpage', 'thesis', 'software'
+    ]
+    const labelProvider = this.context.labelProvider
+
+    let el = $$('ul').addClass('se-new-reference-menu')
+    targetTypes.forEach(item => {
+      el.append(
+        $$('li').addClass('se-type').append(
+          labelProvider.getLabel(item)
+        ).on('click', this._onCreate.bind(this, item))
+      )
+    })
+
+    return el
   }
 
   _toggleNewReferencePopup() {
+    const popup = this.state.popup
     this.extendState({
-      mode: 'create'
+      popup: !popup
     })
   }
 
   _doneEditing() {
     this.extendState({
       mode: undefined
+    })
+  }
+
+  _onCreate(targetType) {
+    let defaults = {}
+    defaults = prefillEntity(targetType, '')
+    this.extendState({
+      mode: 'create',
+      modeProps: {
+        type: targetType,
+        defaults: defaults
+      }
     })
   }
 

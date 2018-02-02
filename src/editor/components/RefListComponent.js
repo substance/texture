29bc2b5fd95ed8d7cedec6fae18ea511365ce6fb
Daniel <beilinson@gmail.com>
2018-02-02 21:13:49 +0300

    Prefill entity helper.

--- src/editor/components/RefListComponent.js
+++ src/editor/components/RefListComponent.js
@@ -2,47 +2,10 @@ import { NodeComponent } from 'substance'
 import ModalDialog from '../../shared/ModalDialog'
 import CreateEntity from '../../entities/CreateEntity'
 import EditEntity from '../../entities/EditEntity'
+import prefillEntity from '../../entities/prefillEntity'
 import RefComponent from './RefComponent'
 import Button from './Button'
 import removeElementAndXrefs from '../../util/removeElementAndXrefs'
-
-function prefillEntity(type, text) {
-  let defaults = {
-    type
-  }
-  if (type === 'person') {
-    let parts = text.split(' ')
-    defaults.surname = parts.pop()
-    defaults.givenNames = parts.join(' ')
-  } else if (type === 'organisation') {
-    defaults.name = text
-  } else if (type === 'book') {
-    defaults.source = text
-  } else if (type === 'journal-article') {
-    defaults.articleTitle = text
-  } else if (type === 'conference-proceeding') {
-    defaults.articleTitle = text
-  } else if (type === 'clinical-report') {
-    defaults.articleTitle = text
-  } else if (type === 'preprint') {
-    defaults.articleTitle = text
-  } else if (type === 'report') {
-    defaults.source = text
-  } else if (type === 'periodical') {
-    defaults.articleTitle = text
-  } else if (type === 'data-publication') {
-    defaults.dataTitle = text
-  } else if (type === 'patent') {
-    defaults.articleTitle = text
-  } else if (type === 'webpage') {
-    defaults.title = text
-  } else if (type === 'thesis') {
-    defaults.articleTitle = text
-  } else if (type === 'software') {
-    defaults.title = text
-  }
-  return defaults
-}
 
 export default class RefListComponent extends NodeComponent {
 
--- src/entities/EditRelationship.js
+++ src/entities/EditRelationship.js
@@ -5,45 +5,7 @@ import EditEntity from './EditEntity'
 import ModalDialog from '../shared/ModalDialog'
 import EntitySelector from './EntitySelector'
 import FormTitle from './FormTitle'
-
-
-function prefillEntity(type, text) {
-  let defaults = {
-    type
-  }
-  if (type === 'person') {
-    let parts = text.split(' ')
-    defaults.surname = parts.pop()
-    defaults.givenNames = parts.join(' ')
-  } else if (type === 'organisation') {
-    defaults.name = text
-  } else if (type === 'book') {
-    defaults.source = text
-  } else if (type === 'journal-article') {
-    defaults.articleTitle = text
-  } else if (type === 'conference-proceeding') {
-    defaults.articleTitle = text
-  } else if (type === 'clinical-report') {
-    defaults.articleTitle = text
-  } else if (type === 'preprint') {
-    defaults.articleTitle = text
-  } else if (type === 'report') {
-    defaults.source = text
-  } else if (type === 'periodical') {
-    defaults.articleTitle = text
-  } else if (type === 'data-publication') {
-    defaults.dataTitle = text
-  } else if (type === 'patent') {
-    defaults.articleTitle = text
-  } else if (type === 'webpage') {
-    defaults.title = text
-  } else if (type === 'thesis') {
-    defaults.articleTitle = text
-  } else if (type === 'software') {
-    defaults.title = text
-  }
-  return defaults
-}
+import prefillEntity from './prefillEntity'
 
 /*
   Used to edit relationhips to other entities.

8c42a50a84f261fee437f539f99aea44a431fac4
Michael Aufreiter <michael@substance.io>
2018-02-06 20:41:11 +0100

    Never use doc.get inside of a transaction.

--- src/util/removeElementAndXrefs.js
+++ src/util/removeElementAndXrefs.js
@@ -7,12 +7,15 @@ export default function removeElementAndXrefs(editorSession, elementId, parentEl
 
   if (xrefs.length === 0 || window.confirm(`Deleting this will affect ${xrefs.length} citations. Are you sure?`)) { // eslint-disable-line
     editorSession.transaction(tx => {
-      let node = doc.get(elementId)
+      let node = tx.get(elementId)
+      // ATTENTION: it is important to nodes from the transaction tx!
+      // Be careful with closures here.
+      parentEl = tx.get(parentEl.id)
       parentEl.removeChild(node)
       tx.delete(node.id)
       // Now update xref targets
       xrefs.forEach((xrefId) => {
-        let xref = doc.get(xrefId)
+        let xref = tx.get(xrefId)
         let idrefs = xref.attr('rid').split(' ')
         idrefs = without(idrefs, elementId)
         xref.setAttribute('rid', idrefs.join(' '))

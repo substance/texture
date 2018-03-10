4375c098a37037f3148859ee7da4359785932b7b
Michael Aufreiter <michael@substance.io>
2018-03-10 15:07:38 +0100

    Open external links in browser window, not Electron.

--- app/app.js
+++ app/app.js
@@ -11,11 +11,21 @@ const darServer = require('dar-server')
 const { FSStorageClient } = darServer
 const url = require('url')
 const path = require('path')
+const remote = require('electron').remote
+const { shell } = remote
+
+
+// HACK: we should find a better solution to intercept window.open calls
+// (e.g. as done by LinkComponent)
+window.open = function(url /*, frameName, features*/) {
+  shell.openExternal(url)
+}
 
 window.addEventListener('load', () => {
   substanceGlobals.DEBUG_RENDERING = platform.devtools
   App.mount({}, window.document.body)
 })
+
 
 class App extends Component {
 

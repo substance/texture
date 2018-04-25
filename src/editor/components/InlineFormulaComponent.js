ed30a74b9b8f6120d32fa0fdf3380ee54f7f2d7a
Michael Aufreiter <michael@substance.io>
2018-04-25 16:45:48 +0200

    Use separate component for TexMath, so model changes lead to a rerender.

--- src/editor/components/InlineFormulaComponent.js
+++ src/editor/components/InlineFormulaComponent.js
@@ -6,15 +6,25 @@ export default class InlineFormulaComponent extends NodeComponent {
   render($$) {
     const node = this.props.node
     // TODO: Find out why node.find('tex-math') returns null here
-    const texMath = node.findChild('tex-math').textContent
-    // TODO: Use KaTeX
-
-    const el = $$('span').addClass('sc-math')
-
+    const texMath = node.findChild('tex-math')
+    const el = $$('span').addClass('sc-inline-formula')
+    el.append(
+      $$(TexMathComponent, {
+        node: texMath
+      })
+    )
     if (this.props.isolatedNodeState) {
       el.addClass('sm-'+this.props.isolatedNodeState)
     }
+    return el
+  }
+}
 
+class TexMathComponent extends NodeComponent {
+  render($$) {
+    const node = this.props.node
+    const texMath = node.textContent
+    const el = $$('span').addClass('sc-math')
     try {
       el.append(
         $$('span').html(katex.renderToString(texMath))

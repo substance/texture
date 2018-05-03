2117b8fcf4a06a5a5b56630721f7fa495a568ec0
Daniel <beilinson@gmail.com>
2018-05-03 23:26:16 +0300

    Introduce insert ext-link command.

---
+++ src/editor/commands/InsertExtLinkCommand.js
@@ -0,0 +1,14 @@
+import { AnnotationCommand } from 'substance'
+
+class InsertExtLinkCommand extends AnnotationCommand {
+  executeCreate(params) {
+    let result = super.executeCreate(params)
+    let editorSession = this._getEditorSession(params)
+    editorSession.transaction((tx) => {
+      tx.setSelection(tx.selection.collapse())
+    })
+    return result
+  }
+}
+
+export default InsertExtLinkCommand

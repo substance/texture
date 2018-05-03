812c6bde0460c250799719ef04222fbbd5b8f64a
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-05-03 12:25:14 +0200

    Use dom.setDoctype()

--- src/converter/r2t/UpdateDocType.js
+++ src/converter/r2t/UpdateDocType.js
@@ -3,13 +3,13 @@ import { TextureArticle, DarArticle } from '../../article'
 export default class UpdateDocType {
 
   import(dom) {
-    dom.setDocType(
+    dom.setDoctype(
       ...TextureArticle.getDocTypeParams()
     )
   }
 
   export(dom) {
-    dom.setDocType(
+    dom.setDoctype(
       ...DarArticle.getDocTypeParams()
     )
   }

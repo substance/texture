b6976017546eb6273e3aaca0312b4e1033d01f56
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-05-08 15:04:38 +0200

    Store the app on window for debugging.

--- web/editor.js
+++ web/editor.js
@@ -3,9 +3,14 @@ import { TextureWebApp } from 'substance-texture'
 
 window.addEventListener('load', () => {
   substanceGlobals.DEBUG_RENDERING = platform.devtools
-  TextureWebApp.mount({
+  let app = TextureWebApp.mount({
     archiveId: getQueryStringParam('archive') || 'kitchen-sink',
     storageType: getQueryStringParam('storage') || 'vfs',
     storageUrl: getQueryStringParam('storageUrl') || '/archives'
   }, window.document.body)
+
+  // put the archive and some more things into global scope, for debugging
+  setTimeout(() => {
+    window.app = app
+  }, 500)
 })

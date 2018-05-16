d5d8cfe9f48c77f0d632d6e01a13011eb3fe31d0
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-05-17 01:00:55 +0200

    Introduce testHelpers.testAsync()

--- test/integration/Persistence.test.js
+++ test/integration/Persistence.test.js
@@ -1,44 +1,33 @@
-import { test } from 'substance-test'
-import { DefaultDOMElement as DOM, platform } from 'substance'
-import { TextureWebApp, TextureArchive, checkArchive } from 'substance-texture'
-import { spy, getMountPoint } from '../testHelpers'
-import { applyNOP, toUnix } from './integrationTestHelpers'
+import { DefaultDOMElement as DOM } from 'substance'
+import { TextureArchive, checkArchive } from 'substance-texture'
+import { spy, getMountPoint, testAsync } from '../testHelpers'
+import { applyNOP, toUnix, createTestApp } from './integrationTestHelpers'
 
-test("Persistence: loading and saving the kitchen-sink article", (t) => {
-  t.plan(2)
-  let app, archive, manuscriptSession
-  let rawArchive
-  _setupApp(t)
-  .then(res => {
-    ({app, archive, manuscriptSession } = res)
-    applyNOP(manuscriptSession)
-    spy(archive.storage, 'write')
-  })
-  .then(() => {
-    return app._save()
-  })
-  .then(() => {
-    rawArchive = archive.storage.write.args[1]
-    return archive.storage.read('kitchen-sink')
-  })
-  .then(originalRawArchive => {
-    // the XML should have actually not changed
-    let originalManuscriptXML = originalRawArchive.resources['manuscript.xml'].data
-    let newManuscriptXML = rawArchive.resources['manuscript.xml'].data
-    // retrieve the rawArchive from the function spy
-    rawArchive.resources = Object.assign({}, originalRawArchive.resources, rawArchive.resources)
-    let err = checkArchive(TextureArchive, rawArchive)
-    let details = err ? err.detail : null
-    t.nil(details, 'There should be no error.')
-    // just pick the body and see if this is the same
-    let oldBodyXML = DOM.parseXML(originalManuscriptXML).find('body').getInnerXML()
-    let newBodyXML = DOM.parseXML(newManuscriptXML).find('body').getInnerXML()
-    // Note: we must make sure to compare strings with comparable line-endings
-    t.equal(toUnix(newBodyXML), toUnix(oldBodyXML))
-  })
+testAsync('Persistence: loading and saving the kitchen-sink article', async (t) => {
+  let res = await _setupApp(t)
+  let {app, archive, manuscriptSession} = res
+  applyNOP(manuscriptSession)
+  spy(archive.storage, 'write')
+  await app._save()
+  let rawArchive = archive.storage.write.args[1]
+  let originalRawArchive = await archive.storage.read('kitchen-sink')
+  // the XML should have actually not changed
+  let originalManuscriptXML = originalRawArchive.resources['manuscript.xml'].data
+  let newManuscriptXML = rawArchive.resources['manuscript.xml'].data
+  // retrieve the rawArchive from the function spy
+  rawArchive.resources = Object.assign({}, originalRawArchive.resources, rawArchive.resources)
+  let err = checkArchive(TextureArchive, rawArchive)
+  let details = err ? err.detail : null
+  t.nil(details, 'There should be no error.')
+  // just pick the body and see if this is the same
+  let oldBodyXML = DOM.parseXML(originalManuscriptXML).find('body').getInnerXML()
+  let newBodyXML = DOM.parseXML(newManuscriptXML).find('body').getInnerXML()
+  // Note: we must make sure to compare strings with comparable line-endings
+  t.equal(toUnix(newBodyXML), toUnix(oldBodyXML))
+  t.end()
 })
 
-function _setupApp(t) {
+function _setupApp (t) {
   let app
   return new Promise((resolve, reject) => {
     let App = createTestApp(resolve, reject)
@@ -52,24 +41,4 @@ function _setupApp(t) {
     let manuscriptSession = archive.getEditorSession('manuscript')
     return { app, archive, manifestSession, manuscriptSession }
   })
-}
-
-
-function createTestApp(resolve, reject) {
-  class App extends TextureWebApp {
-
-    didMount() {
-      super.didMount()
-
-    }
-
-    willUpdateState(newState) {
-      if (newState.archive) {
-        resolve(newState.archive)
-      } else if (newState.error) {
-        reject(newState.error)
-      }
-    }
-  }
-  return App
 }

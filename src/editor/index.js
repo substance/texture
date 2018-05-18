0b2b3136d36495a8eafcf95a0dbb7a2e5d47adb2
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-05-18 02:38:27 +0200

    Tidy up index files.

---
+++ src/editor/index.js
@@ -0,0 +1,4 @@
+export { default as EditorPackage } from './EditorPackage'
+export { default as InsertNodeCommand } from './commands/InsertNodeCommand'
+export { default as TableComponent } from './components/TableComponent'
+export * from './util/index'

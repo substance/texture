243cfdb488a5ebadb64bd2060d19e602caf3e01e
Michael Aufreiter <michael@substance.io>
2018-06-07 16:24:20 +0200

    Implement xref tooltip using ReferencesAPI.

--- src/reader/components/ReaderXrefComponent.js
+++ src/reader/components/ReaderXrefComponent.js
@@ -5,11 +5,9 @@ export default class ReaderXrefComponent extends NodeComponent {
 
   render($$) {
     let node = this.props.node
-    
     let refType = node.getAttribute('ref-type')
     let label = getXrefLabel(node)
-    let el = $$('span').addClass('sc-xref sm-'+refType).append(label)
-
+    let el = $$('span').addClass('sc-reader-xref sm-'+refType).append(label)
     // Add a preview if refType is bibr
     if (refType === 'bibr') {
       el.append(
@@ -20,9 +18,24 @@ export default class ReaderXrefComponent extends NodeComponent {
   }
 
   /*
-    Only for references
+    Render preview only for references.
   */
   _renderPreview($$) {
+    let references = this.context.api.getReferences()
+
+    let el = $$('div').addClass('se-preview')
     let xrefTargets = getXrefTargets(this.props.node)
+    xrefTargets.forEach(refId => {
+      let label = references.getLabel(refId)
+      let html = references.renderReference(refId)
+      el.append(
+        $$('div').addClass('se-ref').append(
+          $$('div').addClass('se-label').append(label),
+          $$('div').addClass('se-text').html(html)
+        ).attr('data-id', refId)
+      )
+    })
+    return el
+
   }
 }

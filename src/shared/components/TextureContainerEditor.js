9cc85d9678215966313ff1d6bfe409f0682cfd98
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-06-08 12:44:32 +0200

    Fix TextureContainerEditor._getNodeComponentClass()

--- src/shared/components/TextureContainerEditor.js
+++ src/shared/components/TextureContainerEditor.js
@@ -5,16 +5,31 @@ import { ContainerEditor } from 'substance'
   for nodes which are not supported yet.
 */
 export default class TextureContainerEditor extends ContainerEditor {
+  // overriding the default implementation, to control the behavior
+  // for nodes without explicitly registered component
   _getNodeComponentClass (node) {
     let ComponentClass = this.getComponent(node.type, 'not-strict')
-    if (!ComponentClass) {
+    if (ComponentClass) {
+      // text components are used directly
+      if (node.isText()) {
+        return ComponentClass
+      // other components are wrapped into an IsolatedNodeComponent
+      // except the component is itself a customized IsolatedNodeComponent
+      } else if (ComponentClass.prototype._isCustomNodeComponent || ComponentClass.prototype._isIsolatedNodeComponent) {
+        return ComponentClass
+      } else {
+        return this.getComponent('isolated-node')
+      }
+    } else {
+      // for text nodes without an component registered explicitly
+      // we use the default text component
       if (node.isText()) {
         return this.getComponent('text-node')
+      // otherwise component for unsupported nodes
       } else {
         return this.getComponent('unsupported')
       }
     }
-    return ComponentClass
   }
 
   _getNodeProps (node) {

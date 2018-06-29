4d391f1d103bafa62360d02d0a75d6a853af1d05
Daniel <beilinson@gmail.com>
2018-06-29 13:48:18 +0200

    Multi select widget.

--- src/forms/index.js
+++ src/forms/index.js
@@ -1,2 +1,3 @@
 export { default as FormsPackage } from './FormsPackage'
 export { default as FormRowComponent } from './FormRowComponent'
+export { default as FormMultiSelectComponent } from './FormMultiSelectComponent'

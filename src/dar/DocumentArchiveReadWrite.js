5edd85064f9f535617569ea76726f28001704e2e
Matthias Einbrodt <matthias.einbrodt@unibz.it>
2018-07-03 01:13:06 +0200

    Added private variables in constructor and assigned values from incoming documentArchiveConfig to them

--- src/dar/DocumentArchiveReadWrite.js
+++ src/dar/DocumentArchiveReadWrite.js
@@ -3,6 +3,12 @@ import DocumentArchiveReadOnly from "./DocumentArchiveReadOnly";
 export default class DocumentArchiveReadWrite extends DocumentArchiveReadOnly {
     constructor(documentArchiveConfig) {
         super(documentArchiveConfig)
-        this.buffer = documentArchiveConfig.getBuffer()
+        
+        this._archiveId = null
+        this._buffer = documentArchiveConfig.getBuffer()
+        this._config = documentArchiveConfig.getArticleConfig()
+        this._pendingFiles = {}
+        this._sessions = null
+        this._upstreamArchive = null
     }
 }

2f99207a18fe0e022c6b761a6f199f536e69de11
Matthias Einbrodt <matthias.einbrodt@unibz.it>
2018-07-03 00:49:42 +0200

    Simplified _loadArchive function due to new ArchiveConfig and DocumentArchiveFactory classes

--- src/WebAppChrome.js
+++ src/WebAppChrome.js
@@ -1,17 +1,13 @@
 /* global vfs */
 import { parseKeyEvent, InMemoryDarBuffer } from 'substance'
+import DocumentArchiveFactory from './dar/DocumentArchiveFactory'
 import StorageClientFactory from './dar/StorageClientFactory'
 import AppChrome from './AppChrome'
 
 export default class WebAppChrome extends AppChrome {
 
-  async _loadArchive(archiveId, context) {
-    let buffer = new InMemoryDarBuffer()
-    let storage = StorageClientFactory.getStorageClient(this.props.storageConfig)
-    let ArchiveClass = this.props.archiveClass
-    let archive = new ArchiveClass(storage, buffer, context, {
-      ArticleConfig: this.props.articleConfig
-    })
+  async _loadArchive(archiveId) {
+    let archive = DocumentArchiveFactory.getDocumentArchive(this.props.documentArchiveConfig)
     return archive.load(archiveId)
   }
 

0c48b4b319b945ef79752a1331d66aeb92fb4d30
Matthias Einbrodt <matthias.einbrodt@unibz.it>
2018-07-03 23:04:24 +0200

    The default archive instance created is now of type TextureArchive +++ Bugfixes

--- src/dar/DocumentArchiveFactory.js
+++ src/dar/DocumentArchiveFactory.js
@@ -1,20 +1,32 @@
 import DocumentArchiveReadOnly from "./DocumentArchiveReadOnly"
 import DocumentArchiveReadWrite from "./DocumentArchiveReadWrite"
 import DocumentArchiveTypes from "./DocumentArchiveTypes"
+import InMemoryDarBuffer  from "./InMemoryDarBuffer"
+import TextureArchive from "../TextureArchive"
 
 export default class DocumentArchiveFactory {
     static getDocumentArchive(documentArchiveConfig) {
         let documentArchive
 
         switch( documentArchiveConfig.getId() ) {
+            
             case DocumentArchiveTypes.READ_ONLY:
-                storageClient = new DocumentArchiveReadOnly(documentArchiveConfig)
+                documentArchive = new DocumentArchiveReadOnly(documentArchiveConfig)
                 break;
+            
             case DocumentArchiveTypes.READ_WRITE:
-                storageClient = new DocumentArchiveReadWrite(documentArchiveConfig)
+                documentArchive = new DocumentArchiveReadWrite(documentArchiveConfig)
                 break;
+            
             default:
-                storageClient = null
+                let storage = documentArchiveConfig.getStorageClient(),
+                    buffer = new InMemoryDarBuffer(),
+                    context = documentArchiveConfig.getContext(),
+                    config = {
+                        ArticleConfig: documentArchiveConfig.getArticleConfig()
+                    }
+
+                documentArchive = new TextureArchive(storage, buffer, context, config);
         }
 
         return documentArchive

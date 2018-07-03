22a4ac925b7ea7600b9932b50bbad09bff5d563d
Matthias Einbrodt <matthias.einbrodt@unibz.it>
2018-07-03 23:12:16 +0200

    Added custom archiveId to load

--- web-rewrite-dar-loading-code/reader.js
+++ web-rewrite-dar-loading-code/reader.js
@@ -1,7 +1,9 @@
 import { TextureReaderWebApp } from "substance-texture"
 
 window.addEventListener('load', () => {
-    let app = TextureReaderWebApp.start()
+    let app = TextureReaderWebApp.start({
+        archiveId: "elife-32671"
+    })
     
     // put the archive and some more things into global scope, for debugging
     setTimeout(() => {

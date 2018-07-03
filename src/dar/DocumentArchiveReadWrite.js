d30b5e24254f3535d733f1ef009685ed326828d1
Matthias Einbrodt <matthias.einbrodt@unibz.it>
2018-07-04 00:28:19 +0200

    Removed duplicate instance fields (are already present in parent class) +++ added VFS check with vfsSaveHook

--- src/dar/DocumentArchiveReadWrite.js
+++ src/dar/DocumentArchiveReadWrite.js
@@ -1,14 +1,16 @@
 import DocumentArchiveReadOnly from "./DocumentArchiveReadOnly";
+import StorageTypes from "./StorageTypes"
+import TextureArchive from "../TextureArchive"
+import vfsSaveHook from "../util/vfsSaveHook"
+
 
 export default class DocumentArchiveReadWrite extends DocumentArchiveReadOnly {
     constructor(documentArchiveConfig) {
         super(documentArchiveConfig)
-        
-        this._archiveId = null
         this._buffer = documentArchiveConfig.getBuffer()
-        this._config = documentArchiveConfig.getArticleConfig()
-        this._pendingFiles = {}
-        this._sessions = null
-        this._upstreamArchive = null
+        
+        if (this._storageConfig.getId() == StorageTypes.VFS ) {
+            vfsSaveHook(this._storage, TextureArchive)
+        }
     }
 }

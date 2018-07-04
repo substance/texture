4cec36e20331107b68c2ee36dad3f0f8be876317
Matthias Einbrodt <matthias.einbrodt@unibz.it>
2018-07-04 18:47:08 +0200

    Updated code due to rewritten logic in TextureArchive class

--- src/util/checkArchive.js
+++ src/util/checkArchive.js
@@ -1,8 +1,32 @@
-export default function checkLoadArchive(ArchiveClass, rawArchive) {
-  let testArchive = new ArchiveClass()
-  try {
-    testArchive._ingest(rawArchive)
-  } catch (error) {
-    return error
-  }
+import EditorSessionsGenerator from "../editor/util/EditorSessionsGenerator"
+import EditorSessionsValidator from "../editor/util/EditorSessionsValidator"
+
+export default function checkLoadArchive(ArchiveClass, documentArchiveConfig, rawArchive) {
+  
+  let testArchive = new ArchiveClass(documentArchiveConfig)
+  testArchive.setUpstreamArchive(rawArchive)
+
+  return new Promise(function(resolve, reject) {
+    Promise.all([
+      EditorSessionsGenerator.generateSessionForManifest(testArchive),
+      EditorSessionsGenerator.generateSessionForPubMeta(testArchive)
+    ])
+      .then(function(sessions) {
+        testArchive.setSession("manifest", sessions[0])
+        testArchive.setSession("pub-meta", sessions[1])
+        return EditorSessionsGenerator.generateSessionsForExistingDocuments(testArchive)
+      })
+      .then(function(documentSessions) {
+        let existingSessions = testArchive.getSessions()
+        let finalSessions = Object.assign(existingSessions, documentSessions)
+        let editorSessionsValidator = new EditorSessionsValidator()
+        return editorSessionsValidator.areSessionsValid(finalSessions)
+      })
+      .then(function(validatedSessions) {
+        resolve(validatedSessions)
+      })
+      .catch(function(errors) {
+        reject(errors)
+      })
+  })
 }
--- src/util/vfsSaveHook.js
+++ src/util/vfsSaveHook.js
@@ -1,24 +1,29 @@
 import { platform } from 'substance'
 import checkArchive from './checkArchive'
 
-export default function vfsSaveHook(storage, ArchiveClass) {
+export default function vfsSaveHook(storage, ArchiveClass, documentArchiveConfig) {
   // monkey patch VfsStorageClient so that we can check if the stored data
   // can be loaded
-  storage.write = (archiveId, rawArchive) => {
-    console.log('Writing archive:', rawArchive) // eslint-disable-line
-    return storage.read(archiveId)
-    .then((originalRawArchive) => {
-      rawArchive.resources = Object.assign({}, originalRawArchive.resources, rawArchive.resources)
-      let err = checkArchive(ArchiveClass, rawArchive)
-      if (err) {
-        if (platform.inBrowser) {
-          window.alert('Exported archive is corrupt!') //eslint-disable-line no-alert
-        }
-        console.error(err.detail)
-        return Promise.reject()
-      } else {
-        return Promise.resolve(true)
-      }
+  storage.write = function(archiveId, rawArchive) {
+    return new Promise(function (resolve, reject) {
+      
+      console.log('Writing archive:', rawArchive) // eslint-disable-line
+      
+      storage.read(archiveId)
+        .then(function(originalRawArchive) {
+          rawArchive.resources = Object.assign({}, originalRawArchive.resources, rawArchive.resources)
+          return checkArchive(ArchiveClass, documentArchiveConfig, rawArchive)
+        })
+        .then(function() {
+          resolve(true)
+        })
+        .catch(function(errors) {
+          if (platform.inBrowser) {
+            window.alert('Exported archive is corrupt!') //eslint-disable-line no-alert
+          }
+          console.error(errors)
+          reject(errors)
+        })
     })
   }
 }

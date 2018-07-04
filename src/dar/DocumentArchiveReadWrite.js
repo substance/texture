b7a5acede310782aaa5c47731316cd5b24c1da89
Matthias Einbrodt <matthias.einbrodt@unibz.it>
2018-07-04 15:25:42 +0200

    Added methods from PersistedDocumentArchive class and refactored them if necessary

--- src/dar/DocumentArchiveReadWrite.js
+++ src/dar/DocumentArchiveReadWrite.js
@@ -4,7 +4,7 @@ import DocumentArchiveReadOnly from "./DocumentArchiveReadOnly";
 import EditorSessionGenerator from "../editor/util/EditorSessionsGenerator"
 
 export default class DocumentArchiveReadWrite extends DocumentArchiveReadOnly {
-    
+
     constructor(documentArchiveConfig) {
         super(documentArchiveConfig)
         this.buffer = documentArchiveConfig.getBuffer()
@@ -96,6 +96,91 @@ export default class DocumentArchiveReadWrite extends DocumentArchiveReadOnly {
                     reject(errors)
                 })
         })
+    }
+
+    removeDocument(documentId) {
+        let session = this._sessions[documentId]
+        this._unregisterFromSession(session)
+        
+        this._sessions["manifest"].transaction(tx => {
+            let documents = tx.find('documents')
+            let docEntry = tx.find(`#${documentId}`)
+            documents.removeChild(docEntry)
+        })
+    }
+
+    renameDocument(documentId, name) {
+        this._sessions["manifest"].transaction(tx => {
+            let docEntry = tx.find(`#${documentId}`)
+            docEntry.attr({
+                name
+            })
+        })
+    }
+
+    save() {
+        if ( !this.hasPendingChanges() ) {
+            console.info('Save: no pending changes.')
+            return Promise.resolve()
+        }
+
+        return this._save(this._archiveId)
+    }
+
+    /**
+     * Save as is implemented as follows.
+     *
+     * 1. clone: copy all files from original archive to new archive (backend)
+     * 2. save: perform a regular save using user buffer (over new archive, including pending
+     * documents and blobs)
+     */
+    saveAs(newArchiveId) {
+        return this._storage.clone(this._archiveId, newArchiveId).then(function() {
+            return this._save(newArchiveId)
+        })
+    }
+
+    /*
+     * Create a raw archive for upload from the changed resources.
+     */
+    _save(archiveId) {
+        let self = this,
+            rawArchive = this._exportChanges(self._sessions, self.buffer)
+
+        // CHALLENGE: we either need to lock the buffer, so that
+        // new changes are interfering with ongoing sync
+        // or we need something pretty smart caching changes until the
+        // sync has succeeded or failed, e.g. we could use a second buffer in the meantime
+        // probably a fast first-level buffer (in-mem) is necessary anyways, even in conjunction with
+        // a slower persisted buffer
+        return self._storage.write(archiveId, rawArchive)
+            .then(function(res) {
+                // TODO: if successful we should receive the new version as response
+                // and then we can reset the buffer
+                res = JSON.parse(res)
+                // console.log('Saved. New version:', res.version)
+                self._buffer.reset(res.version)
+                // After successful save the archiveId may have changed (save as use case)
+                self._archiveId = archiveId
+            }).catch(function(errors) {
+                console.error('Saving failed.', errors)
+            })
+    }
+
+    /*
+     * Uses the current state of the buffer to generate a rawArchive object
+     * containing all changed documents
+     */
+    _exportChanges(sessions, buffer) {
+        let rawArchive = {
+            version: buffer.getVersion(),
+            diff: buffer.getChanges(),
+            resources: {}
+        }
+        this._exportManifest(sessions, buffer, rawArchive)
+        this._exportDocuments(sessions, buffer, rawArchive)
+        this._exportAssets(sessions, buffer, rawArchive)
+        return rawArchive
     }
 
     _registerForAllChanges(sessions) {

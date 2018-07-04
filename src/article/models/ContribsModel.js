0ee6dc5bb933c9dac3bd8b0204e64daf4aef6e56
Daniel <beilinson@gmail.com>
2018-07-04 10:06:39 +0300

    Get affiliations API.

--- src/article/models/ContribsModel.js
+++ src/article/models/ContribsModel.js
@@ -15,6 +15,27 @@ export default class ContribsModel extends DefaultModel{
     return contribIds.map(contribId => this.context.pubMetaDb.get(contribId).toJSON())
   }
 
+  getAffiliations() {
+    const authors = this.getAuthors()
+    const affIds = authors.reduce((affs, author) => {
+      const members = author.members || []
+      const memberAffs = members.reduce((a,m) => {
+        return a.concat(m.affiliations)
+      }, [])
+      let affsList = new Array().concat(author.affiliations, memberAffs)
+      if(author.presentAffiliation) {
+        affsList = affsList.concat(author.presentAffiliation)
+      }
+      affsList.forEach(a => {
+        if(affs.indexOf(a) < 0) {
+          affs.push(a)
+        }
+      })
+      return affs
+    }, [])
+    return affIds.map(affId => this.context.pubMetaDb.get(affId).toJSON())
+  }
+
   /*
     Utility method to render a contrib object
   */

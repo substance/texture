2ca4e06cf558e469412faaa23366e8797ddf79b2
Daniel <beilinson@gmail.com>
2018-07-11 18:03:43 +0200

    Add group via metadata editor.

--- src/article/models/GroupCollectionModel.js
+++ src/article/models/GroupCollectionModel.js
@@ -2,6 +2,10 @@ import DefaultCollectionModel from './DefaultCollectionModel'
 
 export default class GroupCollectionModel extends DefaultCollectionModel {
 
+  addItem(item) {
+    return this._api.addEntity(item, 'group')
+  }
+
   _getCollectionId() {
     return 'groups'
   }

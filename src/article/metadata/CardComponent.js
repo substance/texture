0905c56762dc6ec6b88e8319248921eb6f5d6598
Daniel <beilinson@gmail.com>
2018-07-24 14:32:27 +0300

    Introduce Card component.

---
+++ src/article/metadata/CardComponent.js
@@ -0,0 +1,10 @@
+import { Component } from 'substance'
+
+export default class CardComponent extends Component {
+  render($$) {
+    const children = this.props.children
+
+    return $$('div').addClass('sc-card')
+      .append(children)
+  }
+}

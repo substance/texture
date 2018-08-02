bedf6a71e4e3cee8f38e640e40c4e83864846281
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-08-02 20:16:52 +0200

    Tidy-up EntityDatabase schema.

--- src/article/metadata/EntityDatabase.js
+++ src/article/metadata/EntityDatabase.js
@@ -1,22 +1,29 @@
 import { DocumentNode, Document, without } from 'substance'
 
+const STRING = { type: 'string', default: '' }
+const BOOLEAN = { type: 'boolean', default: false }
+function MANY (...targetTypes) {
+  return { type: ['array', 'id'], targetTypes, default: [] }
+}
+function ONE (...targetTypes) {
+  return { type: 'id', targetTypes, default: null }
+}
+
 export class ArticleRecord extends DocumentNode {}
 
 ArticleRecord.schema = {
   type: 'article-record',
-  volume: { type: 'string', optional: false, default: '' },
-  authors: { type: ['person'], default: [] },
-  editors: { type: ['person'], default: [] },
-  issue: { type: 'string', optional: false, default: '' },
-  fpage: { type: 'string', optional: false, default: '' },
-  lpage: { type: 'string', optional: false, default: '' },
-  pageRange: { type: 'string', optional: false, default: '' },
-  elocationId: { type: 'string', optional: false, default: '' },
-  acceptedDate: { type: 'string', optional: false, default: '' },
-  publishedDate: { type: 'string', optional: false, default: '' },
-  receivedDate: { type: 'string', optional: false, default: '' },
-  revReceivedDate: { type: 'string', optional: false, default: '' },
-  revRequestedDate: { type: 'string', optional: false, default: '' }
+  volume: STRING,
+  issue: STRING,
+  fpage: STRING,
+  lpage: STRING,
+  pageRange: STRING,
+  elocationId: STRING,
+  acceptedDate: STRING,
+  publishedDate: STRING,
+  receivedDate: STRING,
+  revReceivedDate: STRING,
+  revRequestedDate: STRING
 }
 
 // Using an 'abstract base class' to be
@@ -31,100 +38,100 @@ export class Book extends BibliographicEntry {}
 
 Book.schema = {
   type: 'book',
-  authors: { type: ['ref-contrib'], default: [], optional: true },
-  editors: { type: ['ref-contrib'], default: [], optional: true },
-  translators: { type: ['ref-contrib'], default: [], optional: true },
-  title: { type: 'string', optional: true },
-  volume: { type: 'string', optional: true },
-  edition: { type: 'string', optional: true },
-  publisherLoc: { type: 'string', optional: true },
-  publisherName: { type: 'string', optional: true },
-  year: { type: 'string', optional: true },
-  month: { type: 'string', optional: true },
-  day: { type: 'string', optional: true },
-  pageCount: { type: 'string', optional: true },
-  series: { type: 'string', optional: true },
-  doi: { type: 'string', optional: true },
-  isbn: { type: 'string', optional: true },
-  pmid: { type: 'string', optional: true }
+  authors: MANY('ref-contrib'),
+  editors: MANY('ref-contrib'),
+  translators: MANY('ref-contrib'),
+  title: STRING,
+  volume: STRING,
+  edition: STRING,
+  publisherLoc: STRING,
+  publisherName: STRING,
+  year: STRING,
+  month: STRING,
+  day: STRING,
+  pageCount: STRING,
+  series: STRING,
+  doi: STRING,
+  isbn: STRING,
+  pmid: STRING
 }
 
 export class Chapter extends BibliographicEntry {}
 
 Chapter.schema = {
   type: 'chapter',
-  title: { type: 'string', optional: true }, // <chapter-title>
-  containerTitle: { type: 'string', optional: true }, // <source>
-  volume: { type: 'string', optional: true },
-  authors: { type: ['ref-contrib'], default: [], optional: true }, // <person-group person-group-type="author">
-  editors: { type: ['ref-contrib'], default: [], optional: true },
-  translators: { type: ['ref-contrib'], default: [], optional: true },
-  edition: { type: 'string', optional: true },
-  publisherLoc: { type: 'string', optional: true },
-  publisherName: { type: 'string', optional: true },
-  year: { type: 'string', optional: true },
-  month: { type: 'string', optional: true },
-  day: { type: 'string', optional: true },
-  fpage: { type: 'string', optional: true },
-  lpage: { type: 'string', optional: true },
-  pageRange: { type: 'string', optional: true },
-  elocationId: { type: 'string', optional: true },
-  series: { type: 'string', optional: true },
-  doi: { type: 'string', optional: true }, // <pub-id pub-id-type="doi">
-  isbn: { type: 'string', optional: true }, // <pub-id pub-id-type="isbn">
-  pmid: { type: 'string', optional: true } // <pub-id pub-id-type="pmid">
+  title: STRING, // <chapter-title>
+  containerTitle: STRING, // <source>
+  volume: STRING,
+  authors: MANY('ref-contrib'), // <person-group person-group-type="author">
+  editors: MANY('ref-contrib'),
+  translators: MANY('ref-contrib'),
+  edition: STRING,
+  publisherLoc: STRING,
+  publisherName: STRING,
+  year: STRING,
+  month: STRING,
+  day: STRING,
+  fpage: STRING,
+  lpage: STRING,
+  pageRange: STRING,
+  elocationId: STRING,
+  series: STRING,
+  doi: STRING, // <pub-id pub-id-type="doi">
+  isbn: STRING, // <pub-id pub-id-type="isbn">
+  pmid: STRING // <pub-id pub-id-type="pmid">
 }
 
 export class DataPublication extends BibliographicEntry {}
 
 DataPublication.schema = {
   type: 'data-publication',
-  title: { type: 'string', optional: true },
-  authors: { type: ['ref-contrib'], default: [], optional: true },
-  containerTitle: { type: 'string', optional: true }, // <source>
-  year: { type: 'string', optional: true },
-  month: { type: 'string', optional: true },
-  day: { type: 'string', optional: true },
-  accessionId: { type: 'string', optional: true },
-  arkId: { type: 'string', optional: true },
-  archiveId: { type: 'string', optional: true },
-  doi: { type: 'string', optional: true }
+  title: STRING,
+  authors: MANY('ref-contrib'),
+  containerTitle: STRING, // <source>
+  year: STRING,
+  month: STRING,
+  day: STRING,
+  accessionId: STRING,
+  arkId: STRING,
+  archiveId: STRING,
+  doi: STRING
 }
 
 export class MagazineArticle extends BibliographicEntry {}
 
 MagazineArticle.schema = {
   type: 'magazine-article',
-  title: { type: 'string', optional: true },
-  authors: { type: ['ref-contrib'], default: [], optional: true },
-  containerTitle: { type: 'string', optional: true }, // <source>
-  year: { type: 'string', optional: true },
-  month: { type: 'string', optional: true },
-  day: { type: 'string', optional: true },
-  volume: { type: 'string', optional: true },
-  fpage: { type: 'string', optional: true },
-  lpage: { type: 'string', optional: true },
-  pageRange: { type: 'string', optional: true },
-  doi: { type: 'string', optional: true }
+  title: STRING,
+  authors: MANY('ref-contrib'),
+  containerTitle: STRING, // <source>
+  year: STRING,
+  month: STRING,
+  day: STRING,
+  volume: STRING,
+  fpage: STRING,
+  lpage: STRING,
+  pageRange: STRING,
+  doi: STRING
 }
 
 export class NewspaperArticle extends BibliographicEntry {}
 
 NewspaperArticle.schema = {
   type: 'newspaper-article',
-  title: { type: 'string', optional: true },
-  authors: { type: ['ref-contrib'], default: [], optional: true },
-  containerTitle: { type: 'string', optional: true }, // <source>
-  year: { type: 'string', optional: true },
-  month: { type: 'string', optional: true },
-  day: { type: 'string', optional: true },
-  volume: { type: 'string', optional: true },
-  fpage: { type: 'string', optional: true },
-  lpage: { type: 'string', optional: true },
-  pageRange: { type: 'string', optional: true },
-  doi: { type: 'string', optional: true },
-  edition: { type: 'string', optional: true },
-  partTitle: { type: 'string', optional: true }
+  title: STRING,
+  authors: MANY('ref-contrib'),
+  containerTitle: STRING, // <source>
+  year: STRING,
+  month: STRING,
+  day: STRING,
+  volume: STRING,
+  fpage: STRING,
+  lpage: STRING,
+  pageRange: STRING,
+  doi: STRING,
+  edition: STRING,
+  partTitle: STRING
 }
 
 export class Patent extends BibliographicEntry {}
@@ -132,56 +139,56 @@ export class Patent extends BibliographicEntry {}
 Patent.schema = {
   // HACK: trying to merge EntitDb into Article model, avoiding type collision
   type: '_patent',
-  inventors: { type: ['ref-contrib'], default: [], optional: true },
-  assignee: { type: 'string', optional: true },
-  title: { type: 'string', optional: true },
-  containerTitle: { type: 'string', optional: true }, // <source>
-  year: { type: 'string', optional: true },
-  month: { type: 'string', optional: true },
-  day: { type: 'string', optional: true },
-  patentNumber: { type: 'string', optional: true },
-  patentCountry: { type: 'string', optional: true },
-  doi: { type: 'string', optional: true }
+  inventors: MANY('ref-contrib'),
+  assignee: STRING,
+  title: STRING,
+  containerTitle: STRING, // <source>
+  year: STRING,
+  month: STRING,
+  day: STRING,
+  patentNumber: STRING,
+  patentCountry: STRING,
+  doi: STRING
 }
 
 export class JournalArticle extends BibliographicEntry {}
 
 JournalArticle.schema = {
   type: 'journal-article',
-  title: { type: 'string', optional: true },
-  authors: { type: ['ref-contrib'], default: [], optional: true },
-  editors: { type: ['ref-contrib'], default: [], optional: true },
-  containerTitle: { type: 'string', optional: true }, // <source>
-  volume: { type: 'string', optional: true },
-  issue: { type: 'string', optional: true },
-  year: { type: 'string', optional: true },
-  month: { type: 'string', optional: true },
-  day: { type: 'string', optional: true },
-  fpage: { type: 'string', optional: true },
-  lpage: { type: 'string', optional: true },
-  pageRange: { type: 'string', optional: true },
-  elocationId: { type: 'string', optional: true },
-  doi: { type: 'string', optional: true },
-  pmid: { type: 'string', optional: true }
+  title: STRING,
+  authors: MANY('ref-contrib'),
+  editors: MANY('ref-contrib'),
+  containerTitle: STRING, // <source>
+  volume: STRING,
+  issue: STRING,
+  year: STRING,
+  month: STRING,
+  day: STRING,
+  fpage: STRING,
+  lpage: STRING,
+  pageRange: STRING,
+  elocationId: STRING,
+  doi: STRING,
+  pmid: STRING
 }
 
 export class ConferencePaper extends BibliographicEntry {}
 
 ConferencePaper.schema = {
   type: 'conference-paper',
-  title: { type: 'string', optional: true }, // <article-title>
-  authors: { type: ['ref-contrib'], default: [], optional: true },
-  confName: { type: 'string', optional: true },
-  confLoc: { type: 'string', optional: true },
-  containerTitle: { type: 'string', optional: true }, // <source>
-  year: { type: 'string', optional: true },
-  month: { type: 'string', optional: true },
-  day: { type: 'string', optional: true },
-  fpage: { type: 'string', optional: true },
-  lpage: { type: 'string', optional: true },
-  pageRange: { type: 'string', optional: true },
-  elocationId: { type: 'string', optional: true },
-  doi: { type: 'string', optional: true }
+  title: STRING, // <article-title>
+  authors: MANY('ref-contrib'),
+  confName: STRING,
+  confLoc: STRING,
+  containerTitle: STRING, // <source>
+  year: STRING,
+  month: STRING,
+  day: STRING,
+  fpage: STRING,
+  lpage: STRING,
+  pageRange: STRING,
+  elocationId: STRING,
+  doi: STRING
 }
 
 export class Report extends BibliographicEntry {
@@ -192,79 +199,79 @@ export class Report extends BibliographicEntry {
 
 Report.schema = {
   type: 'report',
-  authors: { type: ['ref-contrib'], default: [], optional: true },
-  sponsors: { type: ['ref-contrib'], default: [], optional: true },
-  title: { type: 'string', optional: true },
-  year: { type: 'string', optional: true },
-  month: { type: 'string', optional: true },
-  day: { type: 'string', optional: true },
-  publisherName: { type: 'string', optional: true },
-  publisherLoc: { type: 'string', optional: true },
-  series: { type: 'string', optional: true },
-  isbn: { type: 'string', optional: true },
-  doi: { type: 'string', optional: true }
+  authors: MANY('ref-contrib'),
+  sponsors: MANY('ref-contrib'),
+  title: STRING,
+  year: STRING,
+  month: STRING,
+  day: STRING,
+  publisherName: STRING,
+  publisherLoc: STRING,
+  series: STRING,
+  isbn: STRING,
+  doi: STRING
 }
 
 export class Software extends BibliographicEntry {}
 
 Software.schema = {
   type: 'software',
-  title: { type: 'string', optional: true },
-  authors: { type: ['ref-contrib'], default: [], optional: true },
-  version: { type: 'string', optional: true },
-  publisherLoc: { type: 'string', optional: true },
-  publisherName: { type: 'string', optional: true },
-  year: { type: 'string', optional: true },
-  month: { type: 'string', optional: true },
-  day: { type: 'string', optional: true },
-  doi: { type: 'string', optional: true }
+  title: STRING,
+  authors: MANY('ref-contrib'),
+  version: STRING,
+  publisherLoc: STRING,
+  publisherName: STRING,
+  year: STRING,
+  month: STRING,
+  day: STRING,
+  doi: STRING
 }
 
 export class Thesis extends BibliographicEntry {}
 
 Thesis.schema = {
   type: 'thesis',
-  title: { type: 'string', optional: true },
-  authors: { type: ['ref-contrib'], default: [], optional: true },
-  year: { type: 'string', optional: true },
-  month: { type: 'string', optional: true },
-  day: { type: 'string', optional: true },
-  publisherLoc: { type: 'string', optional: true },
-  publisherName: { type: 'string', optional: true },
-  doi: { type: 'string', optional: true }
+  title: STRING,
+  authors: MANY('ref-contrib'),
+  year: STRING,
+  month: STRING,
+  day: STRING,
+  publisherLoc: STRING,
+  publisherName: STRING,
+  doi: STRING
 }
 
 export class Webpage extends BibliographicEntry {}
 
 Webpage.schema = {
   type: 'webpage',
-  title: { type: 'string', optional: true },
+  title: STRING,
   // E.g. website name, where the page appeared
-  containerTitle: { type: 'string', optional: true }, // <source>
-  authors: { type: ['ref-contrib'], default: [], optional: true },
-  year: { type: 'string', optional: true },
-  month: { type: 'string', optional: true },
-  day: { type: 'string', optional: true },
-  publisherLoc: { type: 'string', optional: true },
-  uri: { type: 'string', optional: true }
+  containerTitle: STRING, // <source>
+  authors: MANY('ref-contrib'),
+  year: STRING,
+  month: STRING,
+  day: STRING,
+  publisherLoc: STRING,
+  uri: STRING
 }
 
 export class Person extends DocumentNode {}
 
 Person.schema = {
   type: 'person',
-  givenNames: { type: 'string', optional: true },
-  surname: { type: 'string', optional: true },
-  prefix: { type: 'string', optional: true },
-  suffix: { type: 'string', optional: true },
-  email: { type: 'string', optional: true },
-  orcid: { type: 'string', optional: true },
-  group: { type: 'group', optional: true },
-  affiliations: { type: ['organisation'], default: [] },
-  awards: { type: ['award'], default: [] },
-  equalContrib: { type: 'boolean', optional: true },
-  corresp: { type: 'boolean', optional: true },
-  deceased: { type: 'boolean', optional: true }
+  givenNames: STRING,
+  surname: STRING,
+  prefix: STRING,
+  suffix: STRING,
+  email: STRING,
+  orcid: STRING,
+  group: ONE('group'),
+  affiliations: MANY('organisation'),
+  awards: MANY('award'),
+  equalContrib: BOOLEAN,
+  corresp: BOOLEAN,
+  deceased: BOOLEAN
 }
 
 /* Holds data for persons and instituions/groups in references */
@@ -272,59 +279,59 @@ export class RefContrib extends DocumentNode {}
 
 RefContrib.schema = {
   type: 'ref-contrib',
-  name: { type: 'string', optional: true }, // either family name or institution name
-  givenNames: { type: 'string', optional: true }
+  name: STRING, // either family name or institution name
+  givenNames: STRING
 }
 
 export class Group extends DocumentNode {}
 
 Group.schema = {
   type: 'group',
-  name: { type: 'string', optional: true },
-  email: { type: 'string', optional: true },
-  affiliations: { type: ['organisation'], default: [] },
-  awards: { type: ['award'], default: [] },
-  equalContrib: { type: 'boolean', optional: true },
-  corresp: { type: 'boolean', optional: true }
+  name: STRING,
+  email: STRING,
+  affiliations: MANY('organisation'),
+  awards: MANY('award'),
+  equalContrib: BOOLEAN,
+  corresp: BOOLEAN
 }
 
 export class Organisation extends DocumentNode {}
 
 Organisation.schema = {
   type: 'organisation',
-  name: { type: 'string', optional: true },
-  division1: { type: 'string', optional: true },
-  division2: { type: 'string', optional: true },
-  division3: { type: 'string', optional: true },
+  name: STRING,
+  division1: STRING,
+  division2: STRING,
+  division3: STRING,
   // Consider switching to address-line1,2,3
-  street: { type: 'string', optional: true },
-  addressComplements: { type: 'string', optional: true },
-  city: { type: 'string', optional: true },
-  state: { type: 'string', optional: true },
-  postalCode: { type: 'string', optional: true },
-  country: { type: 'string', optional: true },
-  phone: { type: 'string', optional: true },
-  fax: { type: 'string', optional: true },
-  email: { type: 'string', optional: true },
-  uri: { type: 'string', optional: true }
+  street: STRING,
+  addressComplements: STRING,
+  city: STRING,
+  state: STRING,
+  postalCode: STRING,
+  country: STRING,
+  phone: STRING,
+  fax: STRING,
+  email: STRING,
+  uri: STRING
 }
 
 export class Award extends DocumentNode {}
 
 Award.schema = {
   type: 'award',
-  institution: { type: 'string', optional: true },
-  fundRefId: { type: 'string', optional: true },
-  awardId: { type: 'string', optional: true }
+  institution: STRING,
+  fundRefId: STRING,
+  awardId: STRING
 }
 
 export class Keyword extends DocumentNode {}
 
 Keyword.schema = {
   type: 'keyword',
-  name: { type: 'string', optional: false, default: '' },
-  category: { type: 'string', optional: false, default: '' },
-  language: { type: 'string', optional: false, default: '' }
+  name: STRING,
+  category: STRING,
+  language: STRING
 }
 
 export class Subject extends DocumentNode {}
@@ -332,9 +339,9 @@ export class Subject extends DocumentNode {}
 Subject.schema = {
   // HACK: trying to merge EntitDb into Article model, avoiding type collision
   type: '_subject',
-  name: { type: 'string', optional: false, default: '' },
-  category: { type: 'string', optional: false, default: '' },
-  language: { type: 'string', optional: false, default: '' }
+  name: STRING,
+  category: STRING,
+  language: STRING
 }
 
 export default class EntityDatabase extends Document {

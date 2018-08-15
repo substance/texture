ba899c15ee583592d0bf7ff2d38c06cf56decec4
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-08-15 15:22:24 +0200

    Tidy up make.

--- make.js
+++ make.js
@@ -32,7 +32,7 @@ if (argv.d) {
   const archiveDir = path.resolve(path.join(__dirname, rootDir))
   darServer.serve(b.server, {
     port,
-    serverUrl: 'http://localhost:'+port,
+    serverUrl: 'http://localhost:' + port,
     rootDir: archiveDir,
     apiUrl: '/archives'
   })
@@ -41,56 +41,56 @@ b.serve({ static: true, route: '/', folder: './dist' })
 
 // Make Targets
 
-b.task('clean', function() {
+b.task('clean', function () {
   b.rm(DIST)
   b.rm(TMP)
   b.rm(APPDIST)
 }).describe('removes all generated files and folders.')
 
 b.task('lib', ['clean', 'build:schema', 'build:assets', 'build:lib'])
-.describe('builds the library bundle.')
+  .describe('builds the library bundle.')
 
 b.task('browser', ['clean', 'build:schema', 'build:assets', 'build:browser'])
-.describe('builds the browser bundle.')
+  .describe('builds the browser bundle.')
 
 b.task('publish', ['clean', 'build:schema', 'build:assets', 'build:lib', 'build:web'])
-.describe('builds the release bundle (library + web).')
+  .describe('builds the release bundle (library + web).')
 
 b.task('web', ['clean', 'build:schema', 'build:assets', 'build:browser', 'build:web'])
-.describe('builds the web bundle (browser + web).')
+  .describe('builds the web bundle (browser + web).')
 
 b.task('app', ['clean', 'build:schema', 'build:assets', 'build:browser', 'build:app'])
-.describe('builds the app bundle (electron app).')
+  .describe('builds the app bundle (electron app).')
 
 b.task('test-nodejs', ['clean', 'build:schema', 'build:test-assets'])
-.describe('prepares everything necessary to run tests in node.')
+  .describe('prepares everything necessary to run tests in node.')
 
 b.task('test-browser', ['clean', 'build:schema', 'build:browser', 'build:test-assets', 'build:test-browser'])
-.describe('builds the test-suite for the browser.')
+  .describe('builds the test-suite for the browser.')
 
 // an alias because in all our other projects it is named this way
 b.task('test:browser', ['test-browser'])
 
 b.task('default', ['publish'])
-.describe('default: publish')
+  .describe('default: publish')
 
 // spawns electron after build is ready
 b.task('run-app', ['app'], () => {
   // Note: `await=false` is important, as otherwise bundler would await this to finish
   fork(b, require.resolve('electron/cli.js'), '.', { verbose: true, cwd: APPDIST, await: false })
 })
-.describe('runs the application in electron.')
+  .describe('runs the application in electron.')
 
 // low-level make targets
 
 b.task('schema:single-jats-file', _singleJATSFile)
 
 b.task('schema:jats', () => {
-  _compileSchema('JATS-archiving', RNG_FILES[0], RNG_SEARCH_DIRS, RNG_FILES.slice(0,1))
+  _compileSchema('JATS-archiving', RNG_FILES[0], RNG_SEARCH_DIRS, RNG_FILES.slice(0, 1))
 })
 
 b.task('schema:dar-article', () => {
-  _compileSchema('TextureArticle', RNG_FILES[1], RNG_SEARCH_DIRS, RNG_FILES.slice(0,2))
+  _compileSchema('TextureArticle', RNG_FILES[1], RNG_SEARCH_DIRS, RNG_FILES.slice(0, 2))
 })
 
 b.task('schema:dar-manifest', () => {
@@ -101,17 +101,17 @@ b.task('schema:texture-article', () => {
 })
 
 b.task('schema:debug', () => {
-  _compileSchema('JATS-archiving', RNG_FILES[0], RNG_SEARCH_DIRS, RNG_FILES.slice(0,1), { debug: true })
-  _compileSchema('TextureArticle', RNG_FILES[1], RNG_SEARCH_DIRS, RNG_FILES.slice(0,2), { debug: true })
+  _compileSchema('JATS-archiving', RNG_FILES[0], RNG_SEARCH_DIRS, RNG_FILES.slice(0, 1), { debug: true })
+  _compileSchema('TextureArticle', RNG_FILES[1], RNG_SEARCH_DIRS, RNG_FILES.slice(0, 2), { debug: true })
 })
 
-b.task('build:assets', function() {
-  b.copy('./node_modules/font-awesome', DIST+'font-awesome')
-  b.copy('./node_modules/katex/dist', DIST+'katex')
-  b.copy('./node_modules/substance/dist', DIST+'substance/dist')
-  b.css('texture.css', DIST+'texture.css')
-  b.css('./node_modules/substance/substance-pagestyle.css', DIST+'texture-pagestyle.css')
-  b.css('./node_modules/substance/substance-reset.css', DIST+'texture-reset.css')
+b.task('build:assets', function () {
+  b.copy('./node_modules/font-awesome', DIST + 'font-awesome')
+  b.copy('./node_modules/katex/dist', DIST + 'katex')
+  b.copy('./node_modules/substance/dist', DIST + 'substance/dist')
+  b.css('texture.css', DIST + 'texture.css')
+  b.css('./node_modules/substance/substance-pagestyle.css', DIST + 'texture-pagestyle.css')
+  b.css('./node_modules/substance/substance-reset.css', DIST + 'texture-reset.css')
 })
 
 b.task('build:schema', ['schema:jats', 'schema:dar-article', 'schema:texture-article', 'schema:dar-manifest'])
@@ -139,47 +139,47 @@ b.task('build:app', () => {
   // FIXME: this command leads to an extra run when a  file is updated
   // .. instead copying the files explicitly for now
   // b.copy('dist', APPDIST+'lib/')
-  b.copy('dist/font-awesome', APPDIST+'lib/')
-  b.copy('dist/katex', APPDIST+'lib/')
-  b.copy('dist/substance', APPDIST+'lib/')
+  b.copy('dist/font-awesome', APPDIST + 'lib/')
+  b.copy('dist/katex', APPDIST + 'lib/')
+  b.copy('dist/substance', APPDIST + 'lib/')
   ;[
     'texture.js',
     'texture.css',
     'texture-pagestyle.css',
     'texture-reset.css'
   ].forEach(f => {
-    b.copy(`dist/${f}`, APPDIST+'lib/')
-    b.copy(`dist/${f}.map`, APPDIST+'lib/')
+    b.copy(`dist/${f}`, APPDIST + 'lib/')
+    b.copy(`dist/${f}.map`, APPDIST + 'lib/')
   })
 
   // TODO: maybe we could come up with an extension
   // that expands a source file using a given dict.
   b.custom('Creating application package.json...', {
     src: 'app/package.json.in',
-    dest: APPDIST+'package.json',
-    execute() {
+    dest: APPDIST + 'package.json',
+    execute () {
       let { version } = require('./package.json')
       let tpl = fs.readFileSync('app/package.json.in', 'utf8')
       let out = tpl.replace('${version}', version)
-      fs.writeFileSync(APPDIST+'package.json', out)
+      fs.writeFileSync(APPDIST + 'package.json', out)
     }
   })
   b.js('app/main.js', {
     output: [{
-      file: APPDIST+'main.js',
+      file: APPDIST + 'main.js',
       format: 'cjs'
     }],
     external: ['electron', 'path', 'url']
   })
   b.js('app/app.js', {
     output: [{
-      file: APPDIST+'app.js',
+      file: APPDIST + 'app.js',
       format: 'umd',
       name: 'textureApp',
       globals: {
-        'substance': 'window.substance',
-        'substance-texture': 'window.texture',
-        'katex': 'window.katex'
+        'substance': 'substance',
+        'substance-texture': 'texture',
+        'katex': 'katex'
       }
     }],
     external: [ 'substance', 'substance-texture', 'katex' ]
@@ -191,8 +191,9 @@ b.task('build:app', () => {
 b.task('build:vfs', () => {
   vfs(b, {
     src: ['./data/**/*'],
-    dest: DIST+'/vfs.js',
-    format: 'umd', moduleName: 'vfs',
+    dest: DIST + '/vfs.js',
+    format: 'umd',
+    moduleName: 'vfs',
     rootDir: path.join(__dirname, 'data')
   })
 })
@@ -202,38 +203,39 @@ b.task('build:web', ['build:vfs'], () => {
   b.copy('web/reader.html', DIST)
   b.js('./web/editor.js', {
     output: [{
-      file: DIST+'editor.js',
+      file: DIST + 'editor.js',
       format: 'umd',
       name: 'textureEditor',
       globals: {
-        'substance': 'window.substance',
-        'substance-texture': 'window.texture',
-        'katex': 'window.katex'
+        'substance': 'substance',
+        'substance-texture': 'texture',
+        'katex': 'katex'
       }
     }],
     external: ['substance', 'substance-texture', 'katex']
   })
   b.js('./web/reader.js', {
     output: [{
-      file: DIST+'reader.js',
+      file: DIST + 'reader.js',
       format: 'umd',
       name: 'textureReader',
       globals: {
-        'substance': 'window.substance',
-        'substance-texture': 'window.texture',
-        'katex': 'window.katex'
+        'substance': 'substance',
+        'substance-texture': 'texture',
+        'katex': 'katex'
       }
     }],
     external: ['substance', 'substance-texture', 'katex']
   })
-  b.copy('./data', DIST+'data')
+  b.copy('./data', DIST + 'data')
 })
 
 b.task('build:test-assets', ['build:vfs-es'], () => {
   vfs(b, {
     src: ['./test/fixture/**/*.xml'],
     dest: './tmp/test-vfs.js',
-    format: 'es', moduleName: 'fixtures'
+    format: 'es',
+    moduleName: 'fixtures'
   })
 })
 
@@ -251,7 +253,7 @@ b.task('build:test-browser', ['build:vfs', 'build:assets', 'build:test-assets'],
   }
   globals[INDEX_JS] = 'texture'
 
-  b.js('test/**/*.test.js', {
+  b.js('test/index.js', {
     output: [{
       file: 'dist/test/tests.js',
       format: 'umd',
@@ -259,8 +261,11 @@ b.task('build:test-browser', ['build:vfs', 'build:assets', 'build:test-assets'],
       globals
     }],
     external: [
-      'substance', 'substance-test', INDEX_JS,
-      'katex', 'vfs'
+      'substance',
+      'substance-test',
+      INDEX_JS,
+      'katex',
+      'vfs'
     ]
   })
 })
@@ -268,7 +273,7 @@ b.task('build:test-browser', ['build:vfs', 'build:assets', 'build:test-assets'],
 b.task('build:vfs-es', () => {
   vfs(b, {
     src: ['./data/**/*'],
-    dest: TMP+'/vfs.es.js',
+    dest: TMP + '/vfs.es.js',
     format: 'es',
     rootDir: path.join(__dirname, 'data')
   })
@@ -276,36 +281,33 @@ b.task('build:vfs-es', () => {
 
 /* HELPERS */
 
-function _buildLib(DEST, platform) {
+function _buildLib (DEST, platform) {
   let targets = []
-  const globals = {
-    'substance': 'substance',
-    'katex': 'katex',
-    'vfs': 'window.vfs'
-  }
   let istanbul
   if (platform === 'browser' || platform === 'all') {
     targets.push({
-      file: DEST+'texture.js',
+      file: DEST + 'texture.js',
       format: 'umd',
       name: 'texture',
       sourcemapRoot: __dirname,
       sourcemapPrefix: 'texture',
-      globals
+      globals: {
+        'substance': 'substance',
+        'katex': 'katex',
+        'vfs': 'vfs'
+      }
     })
   }
   if (platform === 'nodejs' || platform === 'all') {
     targets.push({
-      file: DEST+'texture.cjs.js',
-      format: 'cjs',
-      globals
+      file: DEST + 'texture.cjs.js',
+      format: 'cjs'
     })
   }
   if (platform === 'es' || platform === 'all') {
     targets.push({
-      file: DEST+'texture.es.js',
-      format: 'es',
-      globals
+      file: DEST + 'texture.es.js',
+      format: 'es'
     })
   }
   b.js('./index.js', {
@@ -315,7 +317,7 @@ function _buildLib(DEST, platform) {
   })
 }
 
-function _compileSchema(name, src, searchDirs, deps, options = {} ) {
+function _compileSchema (name, src, searchDirs, deps, options = {}) {
   const DEST = `tmp/${name}.data.js`
   const ISSUES = `tmp/${name}.issues.txt`
   const SCHEMA = `tmp/${name}.schema.md`
@@ -323,7 +325,7 @@ function _compileSchema(name, src, searchDirs, deps, options = {} ) {
   b.custom(`Compiling schema '${name}'...`, {
     src: [src].concat(deps),
     dest: DEST,
-    execute() {
+    execute () {
       const { compileRNG, checkSchema } = require('substance')
       const xmlSchema = compileRNG(fs, searchDirs, entry)
       b.writeFileSync(DEST, `export default ${JSON.stringify(xmlSchema)}`)
@@ -339,16 +341,16 @@ function _compileSchema(name, src, searchDirs, deps, options = {} ) {
 
 // we used this internally just to get a single-file version of
 // the offficial JATS 1.1 rng data set
-function _singleJATSFile() {
+function _singleJATSFile () {
   [{
     RNG_DIR: 'data/jats/archiving',
     ENTRY: 'JATS-archive-oasis-article1-mathml3.rng',
-    DEST: 'src/article/JATS-archiving.rng',
-  }].forEach(({ RNG_DIR, ENTRY, DEST}) => {
+    DEST: 'src/article/JATS-archiving.rng'
+  }].forEach(({ RNG_DIR, ENTRY, DEST }) => {
     b.custom(`Pulling JATS spec into a single file...`, {
-      src: [RNG_DIR+'/*.rng'],
+      src: [RNG_DIR + '/*.rng'],
       dest: DEST,
-      execute() {
+      execute () {
         const { loadRNG } = require('substance')
         let rng = loadRNG(fs, [RNG_DIR], ENTRY)
         // sort definitions by name
@@ -362,7 +364,7 @@ function _singleJATSFile() {
             others.push(child)
           }
         })
-        defines.sort((a,b) => {
+        defines.sort((a, b) => {
           const aname = a.getAttribute('name').toLowerCase()
           const bname = b.getAttribute('name').toLowerCase()
           if (aname < bname) return -1

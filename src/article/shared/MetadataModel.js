3687396a565fafbb3348f7b4bde18e4126b177f0
Daniel <beilinson@gmail.com>
2018-08-17 15:30:43 +0300

    Change the order of sections.

--- src/article/shared/MetadataModel.js
+++ src/article/shared/MetadataModel.js
@@ -5,18 +5,18 @@ export default class MetadataModel extends CompositeModel {
     super(api)
 
     this.setProperties(
-      { name: 'article', model: api._getModelById('article-record') },
       { name: 'authors', model: api.getCollectionForType('author') },
-      { name: 'translations', model: api.getCollectionForType('translatable') },
       { name: 'editors', model: api.getCollectionForType('editor') },
       { name: 'groups', model: api.getCollectionForType('group') },
       { name: 'organisations', model: api.getCollectionForType('organisation') },
       { name: 'awards', model: api.getCollectionForType('award') },
-      { name: 'figures', model: api.getCollectionForType('figure') },
-      { name: 'footnotes', model: api.getCollectionForType('footnote') },
+      // { name: 'footnotes', model: api.getCollectionForType('footnote') },
       { name: 'references', model: api.getCollectionForType('reference') },
       { name: 'keywords', model: api.getCollectionForType('keyword') },
-      { name: 'subjects', model: api.getCollectionForType('subject') }
+      { name: 'subjects', model: api.getCollectionForType('subject') },
+      { name: 'translations', model: api.getCollectionForType('translatable') },
+      { name: 'article', model: api._getModelById('article-record') },
+      { name: 'figures', model: api.getCollectionForType('figure') }
     )
   }
 

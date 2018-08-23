3aee7f2b45513880f24b9cf4a0343f0a2079013d
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-08-23 03:22:34 +0200

    Remove dead code.

--- src/article/models/AuthorCollectionModel.js
+++ src/article/models/AuthorCollectionModel.js
@@ -1,22 +1,9 @@
 import DefaultCollectionModel from './DefaultCollectionModel'
 
 export default class AuthorCollectionModel extends DefaultCollectionModel {
-  constructor (api, node) {
-    super(api, node)
-    this._node = node
-  }
-
   addItem (item = {}) {
     item.type = 'person'
     return this._api.addItemToCollection(item, this)
-  }
-
-  getItems () {
-    return this._node._childNodes.map(id => this._api._getModelById(id))
-  }
-
-  removeItem (item) {
-    this._api.removeItemFromCollection(item, this)
   }
 
   _getCollectionId () {

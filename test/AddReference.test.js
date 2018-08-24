daa659c76d205c32b6a986296210b8df3030613c
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-08-24 16:49:31 +0200

    Fix comment.

--- test/AddReference.test.js
+++ test/AddReference.test.js
@@ -4,7 +4,7 @@ import setupTestApp from './setupTestApp'
 testAsync('Add Reference: TODO do serious stuff here', async (t) => {
   let { app } = await setupTestApp(t)
   let articlePanel = app.find('.sc-article-panel')
-  // open the manuscript panel
+  // open the metadata panel
   articlePanel.send('updateViewName', 'metadata')
   // open the add drop down
   let addDropDown = articlePanel.find('.sc-tool-dropdown.sm-add')

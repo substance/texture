2cca9750833ac52e4b018e5957a3f3d31baf4d0e
Daniel <beilinson@gmail.com>
2018-08-25 22:22:29 +0300

    Open menu and click.

--- test/AddReference.test.js
+++ test/AddReference.test.js
@@ -8,12 +8,9 @@ testAsync('Add Reference: TODO do serious stuff here', async (t) => {
   articlePanel.send('updateViewName', 'metadata')
   // open the add drop down
   let addDropDown = articlePanel.find('.sc-tool-dropdown.sm-add')
-  addDropDown.find('.se-toggle').click()
+  addDropDown.find('button').click()
   // click on the add-reference button
-  // TODO: implement a different check
-  // ATM this click raises an exception, which unfortunately does not yield an exception because the browser consumes exceptions on clicks
-  // but there is not result in that case
-  let res = addDropDown.find('.sm-add-reference').click()
+  addDropDown.find('.sc-menu-item.sm-add-reference').click()
   // TODO: continue here
   t.ok(Boolean(res))
   t.end()

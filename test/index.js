d73b4024ac0d4f695584e80cd935a810b7b5ce54
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-08-29 22:33:59 +0200

    Introduce a test for adding a figure

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 import './testGlobals'
 import './ArticlePanel.test'
 import './AddReference.test'
+import './ManuscriptEditor.test'
 import './Persistence.test'
 import './TableComponent.test'
 // import './converter/index'

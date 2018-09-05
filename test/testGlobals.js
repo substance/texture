fef7158a245f7f89d0b3e29195c00c3ac3dd9a7f
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-09-05 21:01:26 +0200

    Suppress console output in nodejs test run.

--- test/testGlobals.js
+++ test/testGlobals.js
@@ -2,4 +2,8 @@
 // we need to require the bundled file which registers itself as `global.vfs`
 if (typeof global !== 'undefined') {
   require('../dist/vfs.js')
+  // suppress warnings and errors (only in nodejs to have more concise reports)
+  console.info = () => {}
+  console.error = () => {}
+  console.warn = () => {}
 }

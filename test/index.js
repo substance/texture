aeb009c0583f841e50ee570b18fdf9a06856683c
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-09-05 21:01:45 +0200

    Introduce FindAndReplace test.

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 import './testGlobals'
 import './ArticlePanel.test'
 import './AddReference.test'
+import './FindAndReplace.test'
 import './ManuscriptEditor.test'
 import './Persistence.test'
 import './TableComponent.test'

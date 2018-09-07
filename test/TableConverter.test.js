cc5a8847395c334ca8fb40c4d77ed0d80c83f80f
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-09-08 00:02:07 +0200

    Add morw TableConverter tests.

--- test/TableConverter.test.js
+++ test/TableConverter.test.js
@@ -99,6 +99,109 @@ test('TableConverter: export table with col span (1)', t => {
   t.end()
 })
 
+const COL_SPAN_2 = `
+<table>
+  <tr><th>A</th><th>B</th><th>C</th><th>D</th></tr>
+  <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+  <tr><td>5</td><td>6</td><td>7</td><td>8</td></tr>
+  <tr><td>9</td><td>10</td><td>11</td><td>12</td></tr>
+  <tr id="tr5"><td colspan="2">13</td><td>15</td><td>16</td></tr>
+</table>
+`
+
+test('TableConverter: import table with col span (2)', t => {
+  let el = DefaultDOMElement.parseSnippet(COL_SPAN_2.trim(), 'xml')
+  let table = _importTable(el)
+  t.deepEqual(table.getDimensions(), [5, 4], 'table should have correct dimensions')
+  t.ok(table.getChildren().every(row => row.getChildCount() === 4), 'internally every row should have 4 cells')
+  let cellMatrix = table.getCellMatrix()
+  let masterCell = cellMatrix[4][0]
+  let spannedCell = cellMatrix[4][1]
+  t.ok(spannedCell.isShadowed(), 'cell in 5th row and 2nd column should be shadowed')
+  t.ok(spannedCell.getMasterCell() === masterCell, '.. by the cell in the first column')
+  t.end()
+})
+
+test('TableConverter: export table with col span (2)', t => {
+  let el = DefaultDOMElement.parseSnippet(COL_SPAN_2.trim(), 'xml')
+  let table = _importTable(el)
+  let tableEl = _exportTable(table)
+  let tr = tableEl.find('#tr5')
+  let td = tr.getChildAt(0)
+  t.equal(tr.getChildCount(), 3, '5th row should have 3 cells')
+  t.equal(td.getAttribute('colspan'), '2', 'its first cell should have colspan=2')
+  t.end()
+})
+
+const COL_SPAN_3 = `
+<table>
+  <tr><th>A</th><th>B</th><th>C</th><th>D</th></tr>
+  <tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+  <tr><td>5</td><td>6</td><td>7</td><td>8</td></tr>
+  <tr id="tr4"><td colspan="4">9</td></tr>
+  <tr><td>13</td><td>14</td><td>15</td><td>16</td></tr>
+</table>
+`
+
+test('TableConverter: import table with col span (3)', t => {
+  let el = DefaultDOMElement.parseSnippet(COL_SPAN_3.trim(), 'xml')
+  let table = _importTable(el)
+  t.deepEqual(table.getDimensions(), [5, 4], 'table should have correct dimensions')
+  t.ok(table.getChildren().every(row => row.getChildCount() === 4), 'internally every row should have 4 cells')
+  let cellMatrix = table.getCellMatrix()
+  let row4 = cellMatrix[3]
+  let masterCell = cellMatrix[3][0]
+  t.ok(row4.slice(1).every(cell => cell.isShadowed()), 'all cells in 4th should be shadowed')
+  t.notOk(masterCell.isShadowed(), '.. except the first cell')
+  t.ok(row4.slice(1).every(cell => cell.getMasterCell() === masterCell), '.. which should be the master cell of the others')
+  t.end()
+})
+
+test('TableConverter: export table with col span (3)', t => {
+  let el = DefaultDOMElement.parseSnippet(COL_SPAN_3.trim(), 'xml')
+  let table = _importTable(el)
+  let tableEl = _exportTable(table)
+  let tr = tableEl.find('#tr4')
+  let td = tr.getChildAt(0)
+  t.equal(tr.getChildCount(), 1, '4th row should have only one cell')
+  t.equal(td.getAttribute('colspan'), '4', '.. with colspan=4')
+  t.end()
+})
+
+const ROW_SPAN_1 = `
+<table>
+<tr><th>A</th><th>B</th><th>C</th><th>D</th></tr>
+<tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>
+<tr><td>5</td><td>6</td><td>7</td><td>8</td></tr>
+<tr id="tr4"><td>9</td><td rowspan="2">10</td><td>11</td><td>12</td></tr>
+<tr id="tr5"><td>13</td><td>15</td><td>16</td></tr>
+</table>
+`
+
+test('TableConverter: import table with row span (1)', t => {
+  let el = DefaultDOMElement.parseSnippet(ROW_SPAN_1.trim(), 'xml')
+  let table = _importTable(el)
+  t.deepEqual(table.getDimensions(), [5, 4], 'table should have correct dimensions')
+  t.ok(table.getChildren().every(row => row.getChildCount() === 4), 'internally every row should have 4 cells')
+  let cellMatrix = table.getCellMatrix()
+  let row4 = cellMatrix[3]
+  let row5 = cellMatrix[4]
+  t.ok(row5[1].isShadowed(), 'cell in last row and second column should be shadowed')
+  t.ok(row5[1].getMasterCell() === row4[1], 'its master cell should be the cell in the previous row')
+  t.end()
+})
+
+test('TableConverter: export table with row span (1)', t => {
+  let el = DefaultDOMElement.parseSnippet(ROW_SPAN_1.trim(), 'xml')
+  let table = _importTable(el)
+  let tableEl = _exportTable(table)
+  let tr4 = tableEl.find('#tr4')
+  let tr5 = tableEl.find('#tr5')
+  t.equal(tr4.getChildAt(1).getAttribute('rowspan'), '2', 'the second cell in the 4th row should have rowspan=2')
+  t.equal(tr5.getChildCount(), 3, 'the last row should have only 3 children')
+  t.end()
+})
+
 function _importTable (el) {
   // TODO: create a minimal document, and the JATS importer
   // then run the converter and see if the body node has the proper content

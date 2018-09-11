07f8716730ae8ddd5a9c1c219274886fa2501524
Daniel <beilinson@gmail.com>
2018-09-11 16:24:32 +0200

    Figure and table snippets.

---
+++ src/article/ArticleSnippets.js
@@ -0,0 +1,51 @@
+export const FIGURE_SNIPPET = `
+  <fig>
+    <label></label>
+    <caption>
+      <title></title>
+      <p></p>
+    </caption>
+    <graphic xmlns:xlink="http://www.w3.org/1999/xlink" mime-subtype="" mimetype="image" xlink:href="" />
+    <permissions>
+      <copyright-statement></copyright-statement>
+      <copyright-year></copyright-year>
+      <copyright-holder></copyright-holder>
+      <license>
+        <ali:license_ref></ali:license_ref>
+        <license-p></license-p>
+      </license>
+    </permissions>
+  </fig>
+`
+
+export const TABLE_SNIPPET = `
+  <table-wrap id="table1">
+    <label></label>
+    <caption>
+      <title></title>
+      <p></p>
+    </caption>  
+    <table>
+      <tbody>
+        <tr>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </table-wrap>
+`

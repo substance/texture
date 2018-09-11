38b6b59eccfe53e926a4e910713dd6f0fa794413
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-09-11 05:50:16 +0200

    Add HTML converters for heading and codeblocks.

--- src/article/converter/html/ArticleHTMLConverters.js
+++ src/article/converter/html/ArticleHTMLConverters.js
@@ -3,6 +3,8 @@ import TableConverter from '../r2t/TableConverter'
 
 import BoldConverter from './BoldConverter'
 import ExtLinkConverter from './ExtLinkConverter'
+import CodeblockConverter from './CodeblockConverter'
+import HeadingConverter from './HeadingConverter'
 import ItalicConverter from './ItalicConverter'
 import ListConverter from './ListConverter'
 import ParagraphConverter from './ParagraphConverter'
@@ -13,7 +15,9 @@ import UnderlineConverter from './UnderlineConverter'
 
 export default [
   BoldConverter,
+  CodeblockConverter,
   ExtLinkConverter,
+  HeadingConverter,
   ItalicConverter,
   ListConverter,
   ListPackage.ListItemHTMLConverter,

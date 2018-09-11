5afc2651f8fdf9688dfa95bc9e8040cab0bb153e
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-09-11 15:26:45 +0200

    Add test revealing issue #755

--- test/ManuscriptEditor.test.js
+++ test/ManuscriptEditor.test.js
@@ -6,13 +6,11 @@ import setupTestApp from './setupTestApp'
 
 test('ManuscriptEditor: add figure', t => {
   let { app } = setupTestApp(t)
-  let articlePanel = app.find('.sc-article-panel')
-  articlePanel.send('updateViewName', 'manuscript')
-  let editor = articlePanel.find('.sc-manuscript-editor')
+  let editor = _openManuscriptEditor(app)
   setCursor(editor, 'p-2.content', 290)
   // ATTENTION: it is not possible to trigger the file-dialog programmatically
   // instead we are just checking that this does not throw
-  let insertFigureTool = articlePanel.find('.sc-insert-figure-tool')
+  let insertFigureTool = editor.find('.sc-insert-figure-tool')
   t.ok(insertFigureTool.find('button').click(), 'clicking on the insert figure button should not throw')
   // ... and then triggering onFileSelect() directly
   insertFigureTool.onFileSelect(new PseudoFileEvent())
@@ -24,9 +22,7 @@ test('ManuscriptEditor: add figure', t => {
 
 test('ManuscriptEditor: TOC should be updated on change', t => {
   let { app } = setupTestApp(t)
-  let articlePanel = app.find('.sc-article-panel')
-  articlePanel.send('updateViewName', 'manuscript')
-  let editor = articlePanel.find('.sc-manuscript-editor')
+  let editor = _openManuscriptEditor(app)
   let toc = editor.find('.sc-toc')
   editor.context.editorSession.transaction(tx => {
     tx.set(['sec-1', 'content'], 'TEST')
@@ -35,6 +31,35 @@ test('ManuscriptEditor: TOC should be updated on change', t => {
   t.equal(h1.el.text(), 'TEST', 'TOC entry should have been updated')
   t.end()
 })
+
+test('ManuscriptEditor: Switch paragraph to heading', t => {
+  let { app } = setupTestApp(t, { archiveId: 'blank' })
+  let editor = _openManuscriptEditor(app)
+  editor.context.editorSession.transaction(tx => {
+    let body = tx.get('body')
+    body.append(tx.create({
+      type: 'p',
+      id: 'p1'
+    }))
+  })
+  setCursor(editor, 'p1.content', 0)
+  // open the switch type dropdown
+  let switchTypeDropdown = editor.find('.sc-tool-dropdown.sm-text-types')
+  switchTypeDropdown.find('button').click()
+  let h1button = switchTypeDropdown.find('.sc-menu-item.sm-heading1')
+  t.notNil(h1button, 'there should be an option to switch to heading level 1')
+  h1button.click()
+  // ATTENTION: we do not change id, which might be confusing for others
+  let h1El = editor.find('.sc-surface.body > h1')
+  t.notNil(h1El, 'there should be a <h1> element now')
+  t.end()
+})
+
+function _openManuscriptEditor (app) {
+  let articlePanel = app.find('.sc-article-panel')
+  articlePanel.send('updateViewName', 'manuscript')
+  return articlePanel.find('.sc-manuscript-editor')
+}
 
 class PseudoFileEvent {
   constructor () {

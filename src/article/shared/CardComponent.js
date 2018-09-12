4b45f2b9daa2f02847359d1c721ae8cc876662dc
Michael Aufreiter <michael@substance.io>
2018-09-12 20:22:48 +0200

    Prototype styles for card selection.

--- src/article/shared/CardComponent.js
+++ src/article/shared/CardComponent.js
@@ -4,14 +4,22 @@ export default class CardComponent extends Component {
   render ($$) {
     const children = this.props.children
     const label = this.getLabel(this.props.label)
-
     const el = $$('div').addClass('sc-card')
       .append(
         $$('div').addClass('se-label').append(label)
       )
-
+    if (this.state.selected) {
+      el.addClass('sm-selected')
+    }
     el.append(children)
-
+    el.on('click', this._toggleCardSelection)
     return el
+  }
+
+  _toggleCardSelection () {
+    console.log('this.state.selected', this.state.selected)
+    this.setState({
+      selected: !this.state.selected
+    })
   }
 }

e47eee9c7202b2ea6f02cf4a3386c1bfdc29a6ba
Daniel <beilinson@gmail.com>
2018-09-12 12:50:50 +0200

    Provide more article helpers.

--- src/article/articleHelpers.js
+++ src/article/articleHelpers.js
@@ -1,4 +1,4 @@
-import { DefaultDOMElement } from 'substance'
+import { DefaultDOMElement, importNodeIntoDocument, selectionHelpers } from 'substance'
 import createJatsImporter from './converter/r2t/createJatsImporter'
 import { FIGURE_SNIPPET, FOOTNOTE_SNIPPET, TABLE_SNIPPET } from './ArticleSnippets'
 const elementSpippetsMap = {
@@ -13,6 +13,42 @@ export function createEmptyElement (tx, elName) {
     throw new Error('There is no snippet for element', elName)
   }
   let snippetEl = DefaultDOMElement.parseSnippet(snippet.trim(), 'xml')
-  let jatsImporter = createJatsImporter(tx)
-  return jatsImporter.convertElement(snippetEl)
+  let docSnippet = tx.getDocument().createSnippet()
+  let jatsImporter = createJatsImporter(docSnippet)
+  let node = jatsImporter.convertElement(snippetEl)
+  return importNodeIntoDocument(tx, node)
+}
+
+export function setContainerSelection (tx, node) {
+  const p = node.find('p')
+  if (p) {
+    let path = [p.id, 'content']
+    let newSelection = {
+      type: 'property',
+      path,
+      startOffset: 0,
+      surfaceId: node.id
+    }
+    tx.setSelection(newSelection)
+  }
+}
+
+export function importFigure (tx, sel, files, paths) {
+  let LAST = files.length - 1
+  let containerId = sel.containerId
+  files.map((file, idx) => {
+    let path = paths[idx]
+    let mimeData = file.type.split('/')
+    let figure = createEmptyElement(tx, 'figure')
+    let graphic = tx.get(figure.content)
+    graphic.attr({
+      'mime-subtype': mimeData[1],
+      'mimetype': mimeData[0],
+      'xlink:href': path
+    })
+    tx.insertBlockNode(figure)
+    if (idx === LAST) {
+      selectionHelpers.selectNode(tx, figure.id, containerId)
+    }
+  })
 }

88057aa87385f26bcb0620d44e25c250301332fe
Michael Aufreiter <michael@substance.io>
2018-09-18 19:13:01 +0200

    Update display quote component to use models.

--- src/article/shared/DispQuoteComponent.js
+++ src/article/shared/DispQuoteComponent.js
@@ -2,29 +2,28 @@ import { Component } from 'substance'
 
 export default class DispQuoteComponent extends Component {
   render ($$) {
-    const node = this.props.node
-    const ContainerEditor = this.getComponent('container-editor')
-    const TextPropertyEditor = this.getComponent('text-property-editor')
+    let model = this.props.model
+    const contentValueModel = model.getPropertyValue('content')
+    const attribValueModel = model.getPropertyValue('attrib')
+    const ContentEditor = this.getComponent(contentValueModel.type)
+    const AttribEditor = this.getComponent(attribValueModel.type)
 
     let el = $$('div')
       .addClass('sc-disp-quote')
-      .attr('data-id', node.id)
-
-    let quoteContentEl = $$(ContainerEditor, {
-      placeholder: 'Enter Blockquote',
-      node,
-      disabled: this.props.disabled
-    }).ref('editor')
-
-    let attribContentEl = $$(TextPropertyEditor, {
-      placeholder: 'Enter attribution',
-      path: [node.id, 'attrib'],
-      disabled: this.props.disabled
-    }).addClass('se-attribution')
+      .attr('data-id', model.id)
 
     el.append(
-      quoteContentEl,
-      attribContentEl
+      $$(ContentEditor, {
+        model: contentValueModel,
+        placeholder: 'Enter attribution'
+      })
+    )
+
+    el.append(
+      $$(AttribEditor, {
+        model: attribValueModel,
+        placeholder: 'Enter attribution'
+      })
     )
     return el
   }

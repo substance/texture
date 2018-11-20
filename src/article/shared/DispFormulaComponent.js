9d5df2f0f9789f63962649017f6784afe5976a2f
Daniel <beilinson@gmail.com>
2018-11-20 16:00:50 +0300

    Improve disp formula component.

--- src/article/shared/DispFormulaComponent.js
+++ src/article/shared/DispFormulaComponent.js
@@ -1,18 +1,32 @@
 import { Component } from 'substance'
+import { PREVIEW_MODE } from '../ArticleConstants'
+import PreviewComponent from './PreviewComponent'
 import katex from 'katex'
 
 export default class DispFormulaComponent extends Component {
   render ($$) {
+    const node = this.props.node
+    const mode = this.props.mode
     const model = this.props.model
-    const label = model.getLabel()
     const texMath = model.getContent()
-    const el = $$('div').addClass('sc-disp-formula')
-    if (!texMath) {
-      el.append('???')
+    const label = model.getLabel() || '?'
+
+    let el = $$('div')
+      .addClass('sc-disp-formula')
+      .attr('data-id', node.id)
+
+    if (mode === PREVIEW_MODE) {
+      el.append(
+        $$(PreviewComponent, {
+          id: model.id,
+          label: label,
+          description: $$('div').html(katex.renderToString(texMath))
+        })
+      )
     } else {
       try {
         el.append(
-          $$('span').html(katex.renderToString(texMath))
+          $$('span').addClass('se-formula').html(katex.renderToString(texMath))
         )
         let blockerEl = $$('div').addClass('se-blocker')
         el.append(blockerEl)
@@ -20,10 +34,11 @@ export default class DispFormulaComponent extends Component {
         el.addClass('sm-error')
           .text(error.message)
       }
+      el.append(
+        $$('div').addClass('se-label').append(label)
+      )
     }
-    el.append(
-      $$('div').addClass('se-label').append(label)
-    )
+
     return el
   }
 }

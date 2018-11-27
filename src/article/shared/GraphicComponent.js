4bbea105320c565d4fceb0406c36c3fd8b29f6ff
Daniel <beilinson@gmail.com>
2018-11-27 10:06:59 +0300

    Inline graphic error.

--- src/article/shared/GraphicComponent.js
+++ src/article/shared/GraphicComponent.js
@@ -12,15 +12,11 @@ export default class GraphicComponent extends NodeComponent {
     let elClass = mode === 'inline' ? 'sc-inline-graphic' : 'sc-graphic'
     let tagName = mode === 'inline' ? 'span' : 'div'
 
-    let el = $$(tagName).attr('data-id', node.id)
-      .addClass(elClass)
-      .append(
-        $$('img').attr({src: url}).ref('image')
-          .on('error', this._onLoadError)
-      )
+    let el = $$(tagName).addClass(elClass)
+      .attr('data-id', node.id)
 
     if (this.state.errored) {
-      let errorEl = $$('div').addClass('se-error').append(
+      let errorEl = $$(tagName).addClass('se-error').append(
         this.context.iconProvider.renderIcon($$, 'graphic-load-error').addClass('se-icon')
       )
 
@@ -28,9 +24,16 @@ export default class GraphicComponent extends NodeComponent {
         errorEl.append(
           this.getLabel('graphic-load-error')
         )
+      } else {
+        errorEl.attr('title', this.getLabel('graphic-load-error'))
       }
 
       el.append(errorEl)
+    } else {
+      el.append(
+        $$('img').attr({src: url}).ref('image')
+          .on('error', this._onLoadError)
+      )
     }
 
     return el

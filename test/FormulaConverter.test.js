1a3ca69155775229178b612652caf4629a908223
Daniel <beilinson@gmail.com>
2018-11-28 21:21:44 +0300

    Finilize formula converters test.

--- test/FormulaConverter.test.js
+++ test/FormulaConverter.test.js
@@ -12,25 +12,16 @@ const DispFormula = `
 </disp-formula>
 `
 
-const DispFormulaWithoutLabel = `
-<disp-formula id="disp-formula-3">
-  <tex-math><![CDATA[
-1+\frac{q^2}{(1-q)}+\frac{q^6}{(1-q)(1-q^2)}+\cdots=
-\prod_{j=0}^{\infty}\frac{1}{(1-q^{5j+2})(1-q^{5j+3})}, 
-\quad\quad \text{for }\lvert q\rvert<1.]]></tex-math>
-</disp-formula>
-`
-
 const DispFormulaWithoutCDATA = `
 <disp-formula id="disp-formula-3">
   <label>(1)</label>
-  <tex-math>\sqrt(13)</tex-math>
+  <tex-math>sqrt(13)</tex-math>
 </disp-formula>
 `
 
-const TexMathWithId = `<tex-math>\sqrt(13)</tex-math>`
+const TexMathWithId = `<tex-math id="tex-math-1">sqrt(13)</tex-math>`
 
-test('DispFormulaConverter: import', t => {
+test('DispFormulaConverter: importing', t => {
   let el = DefaultDOMElement.parseSnippet(DispFormula.trim(), 'xml')
   let DispFormulaNode = _importElement(el)
   t.equal(DispFormulaNode.id, 'disp-formula-3', 'id should be preserved')
@@ -39,11 +30,29 @@ test('DispFormulaConverter: import', t => {
   t.end()
 })
 
-test('DispFormulaConverter: export', function (t) {
+test('DispFormulaConverter: exporting', function (t) {
   let el = DefaultDOMElement.parseSnippet(DispFormula.trim(), 'xml')
   let DispFormulaNode = _importElement(el)
   let DispFormulaEl = _exportElement(DispFormulaNode)
   t.equal(DispFormulaEl.getOuterHTML(), DispFormula.replace(/\n|\r/g, ''), 'exported element should be equal to imported')
+  t.end()
+})
+
+test('DispFormulaConverter: exported tex-math should always contain CDATA', function (t) {
+  let el = DefaultDOMElement.parseSnippet(DispFormulaWithoutCDATA.trim(), 'xml')
+  let DispFormulaNode = _importElement(el)
+  let DispFormulaEl = _exportElement(DispFormulaNode)
+  let TexMathEl = DispFormulaEl.find('tex-math')
+  t.equal(TexMathEl.getChildCount(), 1, 'tex-math should contain one element')
+  t.equal(TexMathEl.getFirstChild().nodeType, 'cdata', 'tex-math should contain cdata element')
+  t.end()
+})
+
+test('TexMathConverter: exported element should not have id', function (t) {
+  let el = DefaultDOMElement.parseSnippet(TexMathWithId.trim(), 'xml')
+  let TexMathNode = _importElement(el)
+  let TexMathEl = _exportElement(TexMathNode)
+  t.isNil(TexMathEl.getAttribute('id'), 'tex-math should not contain id attribute')
   t.end()
 })
 

2636fcde0746e1ff74d74898e045c109da43eb30
Daniel <beilinson@gmail.com>
2018-12-04 19:05:45 +0300

    Import table footnotes.

--- src/article/converter/r2t/TableWrapConverter.js
+++ src/article/converter/r2t/TableWrapConverter.js
@@ -6,6 +6,22 @@ export default class TableWrapConverter extends FigConverter {
 
   get tagName () { return 'table-wrap' }
 
+  import (el, node, importer) {
+    super.import(el, node, importer)
+    let $$ = el.createElement.bind(el.getOwnerDocument())
+    const TableWrapFoot = findChild(el, 'table-wrap-foot')
+    const FnGroup = findChild(TableWrapFoot, 'fn-group')
+    node.footnotes = []
+    let fnEls = FnGroup.findAll('fn')
+    node.footnotes = fnEls.map(fnEl => {
+      // there must be at least one paragraph
+      if (!fnEl.find('p')) {
+        fnEl.append($$('p'))
+      }
+      return importer.convertElement(fnEl).id
+    })
+  }
+
   _getContent (el) {
     return findChild(el, 'table')
   }

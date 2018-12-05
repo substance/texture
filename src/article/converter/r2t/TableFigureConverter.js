0f017dea2f74fb433b9b378c52dc04af5708e87b
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-12-05 08:52:19 +0100

    Fix TableFigureConverter.

--- src/article/converter/r2t/TableFigureConverter.js
+++ src/article/converter/r2t/TableFigureConverter.js
@@ -10,16 +10,22 @@ export default class TableFigureConverter extends FigureConverter {
     super.import(el, node, importer)
 
     const $$ = el.createElement.bind(el.getOwnerDocument())
+    // table-wrap-foot is optional
     const tableWrapFoot = findChild(el, 'table-wrap-foot')
-    const fnGroup = findChild(tableWrapFoot, 'fn-group')
-    let fnEls = fnGroup.findAll('fn')
-    node.footnotes = fnEls.map(fnEl => {
-      // there must be at least one paragraph
-      if (!fnEl.find('p')) {
-        fnEl.append($$('p'))
+    if (tableWrapFoot) {
+      // fn-group is optional
+      const fnGroup = findChild(tableWrapFoot, 'fn-group')
+      if (fnGroup) {
+        let fnEls = fnGroup.findAll('fn')
+        node.footnotes = fnEls.map(fnEl => {
+          // there must be at least one paragraph
+          if (!fnEl.find('p')) {
+            fnEl.append($$('p'))
+          }
+          return importer.convertElement(fnEl).id
+        })
       }
-      return importer.convertElement(fnEl).id
-    })
+    }
   }
 
   _getContent (el) {

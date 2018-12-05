3f36c8c9467dd8837ee5fd84c44b5515b1101dd7
Daniel <beilinson@gmail.com>
2018-12-05 21:47:39 +0300

    Try to get footnote manager for element.

--- src/article/editor/InsertFootnoteCrossReferenceCommand.js
+++ src/article/editor/InsertFootnoteCrossReferenceCommand.js
@@ -1,4 +1,5 @@
 import { findParentByType } from '../shared/nodeHelpers'
+import { hasAvailableXrefTargets } from '../shared/xrefHelpers'
 import InsertXrefCommand from './InsertXrefCommand'
 
 export default class InsertFootnoteCrossReferenceCommand extends InsertXrefCommand {
@@ -13,11 +14,37 @@ export default class InsertFootnoteCrossReferenceCommand extends InsertXrefComma
     return newState
   }
 
-  detectScope (params, context) {
+  detectScope (params) {
+    const parentTable = this._getParentTable(params)
+    return parentTable ? 'table' : 'manuscript'
+  }
+
+  isDisabled (params, context) {
+    const sel = params.selection
+    const refType = this.config.refType
+    const parentTable = this._getParentTable(params)
+    let hasTargets = false
+    if (parentTable) {
+      const footnoteManager = parentTable._tableFootnoteManager
+      hasTargets = footnoteManager.hasCitables()
+    } else {
+      hasTargets = hasAvailableXrefTargets(refType, context)
+    }
+
+    // don't xref insertion
+    // 1. if the selections is not a collapsed property selection
+    // 2. if there are no citable targets available
+    if (!sel.isPropertySelection() || !sel.isCollapsed() || !hasTargets) {
+      return true
+    }
+    return false
+  }
+
+  _getParentTable (params) {
     const doc = params.editorSession.getDocument()
     const nodeId = params.selection.getNodeId()
     const node = doc.get(nodeId)
     const parentTable = findParentByType(node, 'table-figure')
-    return parentTable ? 'table' : 'manuscript'
+    return parentTable
   }
 }

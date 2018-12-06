19621058c33adbbfc19599b39aa493f84c9e5f25
Daniel <beilinson@gmail.com>
2018-12-06 14:05:35 +0300

    Improve insert footnote command.

--- src/article/shared/InsertFootnoteCommand.js
+++ src/article/shared/InsertFootnoteCommand.js
@@ -1,11 +1,25 @@
+import { findParentByType } from './nodeHelpers'
 // TODO: move AddEntityCommand into shared
 import AddEntityCommand from '../metadata/AddEntityCommand'
 
 export default class InsertFootnoteCommand extends AddEntityCommand {
-  // TODO: implement this command so that it detects the scope 'default' vs 'table-figure
-  // This should be solved similar to what we have in InsertFootnoteCrossReferenceCommand.
+  detectScope (params) {
+    const xpath = params.selectionState.xpath
+    return xpath.indexOf('table-figure') > -1 ? 'table-figure' : 'default'
+  }
 
   _getCollection (params, context) {
-    // TODO: provide the right collection depending on the current state
+    const scope = this.detectScope(params)
+    if (scope === 'default') {
+      const collectionName = 'footnotes'
+      return context.api.getModelById(collectionName)
+    } else {
+      const doc = params.editorSession.getDocument()	
+      const nodeId = params.selection.getNodeId()	
+      const node = doc.get(nodeId)	
+      const parentTable = findParentByType(node, 'table-figure')
+      const tableModel = context.api.getModelById(parentTable.id)
+      return tableModel.getFootnotes()
+    }
   }
 }

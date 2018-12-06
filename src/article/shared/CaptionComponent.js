326e3a4997317d46a327753a32ca447465cea5c9
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-12-06 16:13:57 +0100

    Improve CaptionComponent.

--- src/article/shared/CaptionComponent.js
+++ src/article/shared/CaptionComponent.js
@@ -1,17 +1,18 @@
 import { Component } from 'substance'
 
-// TODO: turn this into a Model based component
+// TODO: turn this into a Model based component, also it should be able to reuse an existing component
 export default class CaptionComponent extends Component {
   render ($$) {
+    const Container = this.getComponent('container')
     const node = this.props.node
     let el = $$('div')
       .addClass('sc-caption')
       .attr('data-id', node.id)
-    let contentEl = $$(this.getComponent('container'), {
+    let contentEl = $$(Container, {
       placeholder: 'Enter Caption',
       node: node,
       disabled: this.props.disabled
-    })
+    }).ref('content')
     el.append(contentEl)
     return el
   }

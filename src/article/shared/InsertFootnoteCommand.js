dd55c452dd998570f57faaef6f7c7da04548da88
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-12-06 10:36:05 +0100

    Add stub for InsertFootnoteCommand.

---
+++ src/article/shared/InsertFootnoteCommand.js
@@ -0,0 +1,9 @@
+// TODO: move AddEntityCommand into shared
+import AddEntityCommand from '../metadata/AddEntityCommand'
+
+export default class InsertFootnoteCommand extends AddEntityCommand {
+
+  // TODO: implement this command so that it detects the scope 'default' vs 'table-figure
+  // This should be solved similar to what we have in InsertFootnoteCrossReferenceCommand.
+
+}

bb71b2ccb64a8cba8faa6a3e00a67dbe976bd91f
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-12-19 10:22:20 +0100

    Remove obsolete code.

--- src/kit/model/CollectionValueModel.js
+++ src/kit/model/CollectionValueModel.js
@@ -1,14 +1,13 @@
 import _ContainerModel from './_ContainerModel'
 
-// Note: at this higher level, 'Container' is not a very good name because ot is not very specific
-// We should come up with a name that makes clear that this is something that could be editied in a classical 'word-processor'.
-// I.e. the content consists of a sequence of TextBlocks as well as blocks with other typically structured content.
-// As a working title I'd suggest to use the term 'flow content' which is used in HTML for exactly this type of content
+// TODO: We need to refactor the general Model API
+// I want to use only ValueModels, and Models that are derived from the schema automatically.
+// Instead if maintaining a model class for every single concept, I want to stream-line the document schema,
+// and use a generated Model API. For instance, the panels of a figure will not be an extra node, and a Figure is
+// would either be a Collection of FigurePanel, or it would own a property 'panels' of type collection.
+// I'd prefer the latter, because it is more general. Then, instead of figure.addPanel()
+// one would need to use figure.getPanels().addItem()
 export default class CollectionValueModel extends _ContainerModel {
-  get type () { return 'collection' }
-
-  get id () { return this._path.join('.') }
-
   getItems () {
     return this._getItems()
   }
@@ -19,45 +18,5 @@ export default class CollectionValueModel extends _ContainerModel {
     if (id) {
       return this._api.getModelById(id)
     }
-  }
-
-  addItem (item) {
-    return this._api.addItemToCollection(this._prepareItem(item), this)
-  }
-
-  addItems (items) {
-    return this._api.addItemsToCollection(items.map(item => this._prepareItem(item)), this)
-  }
-
-  removeItem (item) {
-    this._api.removeItemFromCollection(item, this)
-  }
-
-  // TODO: consolidate Collection interface
-
-  get isCollection () {
-    return true
-  }
-
-  // TODO: change naming. Is the collection removable? probably rather an item. If it an item is removable the collection is mutable.
-  get isRemovable () {
-    return false
-  }
-
-  // TODO: change naming. Again, this sounds more like item isMovable() which in terms of the collection could be described isOrdered()
-  // i.e. user can change order
-  get isMovable () {
-    return false
-  }
-
-  _prepareItem (item) {
-    if (!item.type) {
-      item.type = this._getItemType()
-    }
-    return item
-  }
-
-  _getItemType () {
-    return this._targetTypes[0]
   }
 }

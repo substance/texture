f120c8f758927fea6bc7cecf2a83072195fb92d9
Daniel <beilinson@gmail.com>
2018-12-19 16:00:17 +0100

    Improve SupplementaryFile component.

--- src/article/shared/SupplementaryFileComponent.js
+++ src/article/shared/SupplementaryFileComponent.js
@@ -5,13 +5,19 @@ export default class SupplementaryFileComponent extends NodeComponent {
   render ($$) {
     const model = this.props.model
     const node = model._node
-    let el = $$('div').addClass('sc-supplementary-file')
-    el.append($$('div').text('TODO: Implement SupplementaryFileComponent'))
-    el.append($$('div').text('href:' + node.href))
-    el.append($$('div').text('legend:'))
-    el.append(
-      renderModelComponent(this.context, $$, { model: model.getLegend() }).ref('legend')
+    const SectionLabel = this.getComponent('section-label')
+    let el = $$('div').addClass('sc-supplementary-file').append(
+      $$('div').addClass('se-header').append(
+        $$('div').addClass('se-label').text(node.label),
+        $$('div').addClass('se-href').text(node.href)
+      ),
+      $$(SectionLabel, {label: 'caption-label'}),
+      renderModelComponent(this.context, $$, {
+        label: this.getLabel('caption'),
+        model: model.getLegend()
+      }).ref('legend')
     )
+
     return el
   }
 }

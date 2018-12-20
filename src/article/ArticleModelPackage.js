778964a52e16e80a0158abf16de97421568a547c
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-12-20 12:31:55 +0100

    Fix registration of SupplementaryManager.

--- src/article/ArticleModelPackage.js
+++ src/article/ArticleModelPackage.js
@@ -85,7 +85,7 @@ export default {
       and: ',',
       to: '-'
     })
-    config.setLabelGenerator('supplementaries', {
+    config.setLabelGenerator('supplementaries', NumberedLabelGenerator, {
       name: 'Supplementary Files',
       plural: 'Supplementary Files',
       and: ',',
--- src/article/shared/SupplementaryManager.js
+++ src/article/shared/SupplementaryManager.js
@@ -2,7 +2,7 @@ import CitableContentManager from './CitableContentManager'
 
 export default class SupplementaryManager extends CitableContentManager {
   constructor (documentSession, labelGenerator) {
-    super(documentSession, 'file', labelGenerator)
+    super(documentSession, 'file', ['supplementary-file'], labelGenerator)
     this._updateLabels('initial')
   }
 }

b95889a7a69563857d9791699a32e7f4fa46a5e2
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-01-15 07:47:38 +0100

    Test switch view toggle.

--- test/ArticlePanel.test.js
+++ test/ArticlePanel.test.js
@@ -1,11 +1,26 @@
 import { test } from 'substance-test'
 import setupTestApp from './shared/setupTestApp'
+import { openManuscriptEditor, openMetadataEditor } from './shared/integrationTestHelpers'
+
+const SWITCH_MODE_TOOL_DROPDOWN_SELECTOR = '.sc-tool-dropdown.sm-mode'
 
 test('ArticlePanel: open every view', t => {
   let { app } = setupTestApp(t)
-  let articlePanel = app.find('.sc-article-panel')
-  articlePanel.send('updateViewName', 'manuscript')
-  articlePanel.send('updateViewName', 'metadata')
+  openManuscriptEditor(app)
+  openMetadataEditor(app)
   t.pass('ArticlePanel should be able to open all views without errors.')
+  t.end()
+})
+
+test('ArticlePanel: using switch view', t => {
+  let { app } = setupTestApp(t)
+  openManuscriptEditor(app)
+  let dropdown = app.find(SWITCH_MODE_TOOL_DROPDOWN_SELECTOR)
+  dropdown.find('button.se-toggle').click()
+  openMetadataEditor(app)
+  dropdown.find('button.sm-open-metadata').click()
+  let metadataEditor = app.find('.sc-metadata-editor')
+  t.notNil(metadataEditor, 'metadata editor should be displayed now')
+  t.equal(metadataEditor.context.appState.viewName, 'metadata', 'appState.viewName should be correct')
   t.end()
 })

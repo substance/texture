5de04fe1d061bb281e143fcf1509240b1d12047d
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-01-24 05:34:31 +0100

    Fix TableCellComponent.

--- src/article/shared/TableCellComponent.js
+++ src/article/shared/TableCellComponent.js
@@ -8,11 +8,15 @@ export default class TableCellComponent extends NodeComponent {
     el.addClass('sc-table-cell')
     el.attr({
       'data-id': cell.id,
-      rowspan: cell.rowspan,
-      colspan: cell.colspan,
       'data-row-idx': cell.rowIdx,
       'data-col-idx': cell.colIdx
     })
+    if (cell.rowspan > 1) {
+      el.attr('rowspan', cell.rowspan)
+    }
+    if (cell.colspan > 1) {
+      el.attr('colspan', cell.colspan)
+    }
     el.append(
       $$(TableCellEditor, {
         path: cell.getPath(),

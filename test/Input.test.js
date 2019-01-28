82e31e207346dce5135758bf5fad45b91e91eaf6
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-01-28 03:20:06 +0100

    Split tests for Single- and ManyRelationshipInput

--- test/Input.test.js
+++ test/Input.test.js
@@ -2,7 +2,7 @@ import { test } from 'substance-test'
 import setupTestApp from './shared/setupTestApp'
 import { openMetadataEditor, createTestVfs } from './shared/integrationTestHelpers'
 
-const TWO_GROUPS = `<?xml version="1.0" encoding="UTF-8"?>
+const AUTHOR_AND_GROUP = `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE article PUBLIC "-//NLM//DTD JATS (Z39.96) Journal Archiving DTD v1.0 20120330//EN" "JATS-journalarchiving.dtd">
 <article xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ali="http://www.niso.org/schemas/ali/1.0">
   <front>
@@ -35,13 +35,14 @@ const TWO_GROUPS = `<?xml version="1.0" encoding="UTF-8"?>
 </article>
 `
 
-test('Input: ManyRelationship dropdown', t => {
+// Note: using Author + Group here
+test('Input: SinlgeRelationship dropdown', t => {
   let { app } = setupTestApp(t, {
-    vfs: createTestVfs(TWO_GROUPS),
+    vfs: createTestVfs(AUTHOR_AND_GROUP),
     archiveId: 'test'
   })
   let metadataEditor = openMetadataEditor(app)
-  let selectInput = metadataEditor.find('.sm-person .sm-group .sc-many-relationship .sc-multi-select-input')
+  let selectInput = metadataEditor.find('.sm-person .sm-group .sc-single-relationship .sc-multi-select-input')
   t.ok(selectInput.el.is('.sm-collapsed'), 'the dropdown should be collapsed in the beginning')
   // click on the input to open the dropdown
   selectInput.el.click()
@@ -58,5 +59,71 @@ test('Input: ManyRelationship dropdown', t => {
   item.click()
   item = selectInput.find('.se-select-item')
   t.ok(item.el.is('.sm-selected'), 'item should be checked')
+  t.end()
+})
+
+const AUTHOR_AND_TWO_AFFS = `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE article PUBLIC "-//NLM//DTD JATS (Z39.96) Journal Archiving DTD v1.0 20120330//EN" "JATS-journalarchiving.dtd">
+<article xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ali="http://www.niso.org/schemas/ali/1.0">
+  <front>
+    <article-meta>
+      <title-group>
+        <article-title></article-title>
+      </title-group>
+      <contrib-group content-type="author">
+        <contrib contrib-type="person" equal-contrib="yes" corresp="yes" deceased="no">
+          <name>
+            <surname>Doe</surname>
+            <given-names>John</given-names>
+          </name>
+          <email>john.doe@university</email>
+          <xref ref-type="aff" rid="aff1" />
+          <xref ref-type="aff" rid="aff2" />
+        </contrib>
+      </contrib-group>
+      <aff id="aff1">
+        <institution content-type="orgname">German Primate Center GmbH</institution>
+        <institution content-type="orgdiv1">Neurobiology Laboratory</institution>
+        <city>Göttingen</city>
+        <country>Germany</country>
+      </aff>
+      <aff id="aff2">
+        <institution content-type="orgname">The Rockefeller University</institution>
+        <institution content-type="orgdiv1">Laboratory of Neural Systems</institution>
+        <city>New York</city>
+        <country>United States</country>
+      </aff>
+    </article-meta>
+  </front>
+  <body>
+  </body>
+  <back>
+  </back>
+</article>
+`
+
+test('Input: ManyRelationship dropdown', t => {
+  let { app } = setupTestApp(t, {
+    vfs: createTestVfs(AUTHOR_AND_TWO_AFFS),
+    archiveId: 'test'
+  })
+  let metadataEditor = openMetadataEditor(app)
+  let selectInput = metadataEditor.find('.sm-person .sm-affiliations .sc-many-relationship .sc-multi-select-input')
+  t.ok(selectInput.el.is('.sm-collapsed'), 'the dropdown should be collapsed in the beginning')
+  // click on the input to open the dropdown
+  selectInput.click()
+  t.ok(selectInput.el.is('.sm-expanded'), 'the dropdown should be expanded')
+  let items = selectInput.findAll('.se-select-item')
+  t.equal(items.length, 2, 'there should be two items to select')
+  let first = items[0]
+  // toggle the first
+  first.click()
+  // we want that the dropdown remains open
+  t.ok(selectInput.el.is('.sm-expanded'), 'the dropdown should still be expanded')
+  // and the specific item should be selected now
+  t.notOk(first.el.is('.sm-selected'), 'item should not be checked')
+  // toggle again
+  first.click()
+  t.ok(first.el.is('.sm-selected'), 'item should be checked')
   t.end()
 })

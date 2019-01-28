adccb23a710db0d9ead6a4b0d608fabb00878c08
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-01-28 04:30:47 +0100

    Fix DownloadSupplementaryFileTool

--- src/article/shared/DownloadSupplementaryFileTool.js
+++ src/article/shared/DownloadSupplementaryFileTool.js
@@ -1,13 +1,15 @@
+import { platform, domHelpers } from 'substance'
 import { Tool } from '../../kit'
 
 export default class DownloadSupplementaryFileTool extends Tool {
   render ($$) {
     let el = super.render($$)
-
-    const link = $$('a')
-      .ref('link')
-
-    el.append(link)
+    el.append(
+      $$('a').ref('link')
+        .attr('target', '_blank')
+        // ATTENTION: stop propagation, otherwise infinite loop
+        .on('click', domHelpers.stop)
+    )
     return el
   }
 
@@ -20,18 +22,14 @@ export default class DownloadSupplementaryFileTool extends Tool {
     e.preventDefault()
     const url = this._generateUrlLink()
     if (url) {
-      // TODO: we should do it rather via our components API,
-      // but for now it is not working:
-      // this.refs.link.el.attr({
-      //   'href': url,
-      // })
-      // this.refs.link.el.click()
-      const a = document.createElement('a')
-      a.href = url
-      document.body.appendChild(a)
-      a.click()
-      document.body.removeChild(a)
-      window.URL.revokeObjectURL(url)
+      this.refs.link.el.attr({
+        'href': url
+      })
+      this.refs.link.el.click()
+
+      if (platform.inBrowser) {
+        window.URL.revokeObjectURL(url)
+      }
     }
   }
 
@@ -43,15 +41,25 @@ export default class DownloadSupplementaryFileTool extends Tool {
     if (remote) {
       return node.href
     } else {
-      const fileId = node.href.split('.')[0]
-      if (fileId) {
-        const archive = this.context.archive
-        const blob = archive.buffer.getBlob(fileId)
+      const archive = this.context.archive
+      if (archive.hasAsset(node.href)) {
+        const blob = archive.getBlob(node.href)
         if (blob) {
-          return window.URL.createObjectURL(blob.blob)
+          if (platform.inBrowser) {
+            return window.URL.createObjectURL(blob.blob)
+          } else {
+            // TODO: what to do in node? maybe a file:// URL?
+            return node.href
+          }
+        } else {
+          // TODO: Some error should be displayed to the user
+          console.error('Blob missing for a file', node.href)
         }
+      } else {
+        // TODO: this should not only be on the console, but displayed as an error
+        // maybe it should be done as a validator task in future
+        console.error('File not found in archive: ' + node.href)
       }
-      console.error('Blob missed for a file', node.href)
     }
   }
 }

4c9beb59e74be1bb9a3990ff9a7077b67bde561a
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-01-28 14:19:15 +0100

    Fix DownloadSupplementaryFileTool

--- src/article/shared/DownloadSupplementaryFileTool.js
+++ src/article/shared/DownloadSupplementaryFileTool.js
@@ -1,4 +1,4 @@
-import { platform, domHelpers } from 'substance'
+import { domHelpers } from 'substance'
 import { Tool } from '../../kit'
 
 export default class DownloadSupplementaryFileTool extends Tool {
@@ -20,46 +20,24 @@ export default class DownloadSupplementaryFileTool extends Tool {
   _onClick (e) {
     e.stopPropagation()
     e.preventDefault()
-    const url = this._generateUrlLink()
+    this._triggerDownload()
+  }
+
+  _triggerDownload () {
+    const archive = this.context.archive
+    const editorSession = this.context.editorSession
+    const selectionState = editorSession.getSelectionState()
+    const node = selectionState.node
+    const isLocal = !node.remote
+    let url = node.href
+    if (isLocal) {
+      url = archive.getDownloadLink(node.href)
+    }
     if (url) {
       this.refs.link.el.attr({
         'href': url
       })
       this.refs.link.el.click()
-
-      if (platform.inBrowser) {
-        window.URL.revokeObjectURL(url)
-      }
-    }
-  }
-
-  _generateUrlLink () {
-    const editorSession = this.context.editorSession
-    const selectionState = editorSession.getSelectionState()
-    const node = selectionState.node
-    const remote = node.remote
-    if (remote) {
-      return node.href
-    } else {
-      const archive = this.context.archive
-      if (archive.hasAsset(node.href)) {
-        const blob = archive.getBlob(node.href)
-        if (blob) {
-          if (platform.inBrowser) {
-            return window.URL.createObjectURL(blob.blob)
-          } else {
-            // TODO: what to do in node? maybe a file:// URL?
-            return node.href
-          }
-        } else {
-          // TODO: Some error should be displayed to the user
-          console.error('Blob missing for a file', node.href)
-        }
-      } else {
-        // TODO: this should not only be on the console, but displayed as an error
-        // maybe it should be done as a validator task in future
-        console.error('File not found in archive: ' + node.href)
-      }
     }
   }
 }

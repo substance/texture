ba3b5672950c234d567647d6fd4ab5aff3c30010
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-01-29 07:17:09 +0100

    Manage SupplementaryFile labels only in body.

--- src/article/shared/SupplementaryManager.js
+++ src/article/shared/SupplementaryManager.js
@@ -5,4 +5,10 @@ export default class SupplementaryManager extends CitableContentManager {
     super(documentSession, 'file', ['supplementary-file'], labelGenerator)
     this._updateLabels('initial')
   }
+
+  // ATTENTION: for now we consider only supplementary files that are direct children of the body
+  // TODO: we need to specify how this should be extended to supplementary files in figure panels
+  getCitables () {
+    return this._getContentElement().resolve('content').filter(child => child.type === 'supplementary-file')
+  }
 }

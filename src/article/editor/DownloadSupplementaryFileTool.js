39fc11339647f1f220457f37dfbff9159e9537f1
Daniel <beilinson@gmail.com>
2019-02-03 21:12:27 +0300

    Set download attribute on download link.

--- src/article/editor/DownloadSupplementaryFileTool.js
+++ src/article/editor/DownloadSupplementaryFileTool.js
@@ -5,6 +5,9 @@ export default class DownloadSupplementaryFileTool extends Tool {
   render ($$) {
     let el = super.render($$)
     let link = $$('a').ref('link')
+      // Note: download attribute instructs browsers to download a URL instead of navigating to it
+      // but only for same-origin URLs, e.g. it will not work for remote images
+      .attr('download', '')
       // ATTENTION: stop propagation, otherwise infinite loop
       .on('click', domHelpers.stop)
     // Note: in the browser version we want to open the download in a new tab

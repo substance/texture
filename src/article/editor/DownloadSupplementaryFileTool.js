7ebb8caa9646dc8293edbee3f3f5d8fd7657d354
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-02-05 06:14:40 +0100

    Improve DownloadSupplementaryFileTool.

--- src/article/editor/DownloadSupplementaryFileTool.js
+++ src/article/editor/DownloadSupplementaryFileTool.js
@@ -5,10 +5,25 @@ export default class DownloadSupplementaryFileTool extends Tool {
   render ($$) {
     let el = super.render($$)
     let link = $$('a').ref('link')
-      // Downloads a non-remote urls
-      .attr('download', '')
       // ATTENTION: stop propagation, otherwise infinite loop
       .on('click', domHelpers.stop)
+
+    // Downloading is a bit involved:
+    // In electron, everything can be done with one solution,
+    // handling a 'will-download' event, which is triggered when the `download`
+    // attribute is present.
+    // For the browser, the `download` attribute works only for files from the same
+    // origin. For remote files the best we can do at the moment, is opening
+    // a new tab, and let the browser deal with it.
+    // TODO: if this feature is important, one idea is that the DAR server could
+    // provide an end-point to provide download-urls, and act as a proxy to
+    // cirvumvent the CORS problem.
+    const isLocal = this._isLocal()
+    if (platform.inElectron || isLocal) {
+      link.attr('download', '')
+    } else {
+      link.attr('target', '_blank')
+    }
 
     el.append(link)
     return el
@@ -26,10 +41,8 @@ export default class DownloadSupplementaryFileTool extends Tool {
 
   _triggerDownload () {
     const archive = this.context.archive
-    const editorSession = this.context.editorSession
-    const selectionState = editorSession.getSelectionState()
-    const node = selectionState.node
-    const isLocal = !node.remote
+    const node = this._getNode()
+    const isLocal = this._isLocal()
     let url = node.href
     if (isLocal) {
       url = archive.getDownloadLink(node.href)
@@ -38,11 +51,16 @@ export default class DownloadSupplementaryFileTool extends Tool {
       this.refs.link.el.attr({
         'href': url
       })
-      // Note: in the browser version we want to open remote files in a new tab
-      if (!platform.inElectron && !isLocal) {
-        this.refs.link.attr('target', '_blank')
-      }
       this.refs.link.el.click()
     }
+  }
+
+  _getNode () {
+    return this.props.commandState.node
+  }
+
+  _isLocal () {
+    let node = this._getNode()
+    return (!node || !node.remote)
   }
 }

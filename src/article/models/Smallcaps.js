011b5253fea8e65881abd0fbf76f806dae607970
Daniel <beilinson@gmail.com>
2019-02-12 22:11:07 +0300

    Rename smallcaps model.

--- src/article/models/Smallcaps.js
+++ src/article/models/Smallcaps.js
@@ -1,6 +1,6 @@
 import Annotation from './Annotation'
 
-export default class Smallcaps extends Annotation {}
-Smallcaps.schema = {
-  type: 'smallcaps'
+export default class SmallCaps extends Annotation {}
+SmallCaps.schema = {
+  type: 'small-caps'
 }

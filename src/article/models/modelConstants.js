d1988b8a0b54ffc65b2d0961b4e5e4a93d6637b5
Daniel <beilinson@gmail.com>
2019-02-13 00:24:46 +0300

    Move small caps to extended formatting.

--- src/article/models/modelConstants.js
+++ src/article/models/modelConstants.js
@@ -1,7 +1,7 @@
 // annotations that are simple annotations
-export const RICH_TEXT_ANNOS = ['bold', 'italic', 'small-caps', 'superscript', 'subscript']
+export const RICH_TEXT_ANNOS = ['bold', 'italic', 'superscript', 'subscript']
 
-export const EXTENDED_FORMATTING = ['monospace', 'strike-through', 'underline', 'overline']
+export const EXTENDED_FORMATTING = ['monospace', 'small-caps', 'strike-through', 'underline', 'overline']
 
 export const LINKS_AND_XREFS = ['xref', 'external-link']
 

5f3f4aa7cff4bf540a17392b419029cf8c45df5d
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-02-18 15:11:47 +0100

    Remove dead code.

--- src/kit/model/addModelObserver.js
+++ src/kit/model/addModelObserver.js
@@ -1,29 +1,7 @@
-// TODO: We should only have nodes or values as models
+// This is only used for Value models
 export default function addModelObserver (model, fn, comp, options = {}) {
   let stage = options.stage || 'render'
-  // NodeModels and alike
-  if (model._isNode) {
-    // NOTE: here we exploit internal knowledge about node types and only register
-    // for updates that are relevant on this level
-    // E.g., for an XMLElementNode we are only interested in changes to `<id>._childNodes`
-    // TODO: allow to register for multiple paths, e.g. `childNodes` and `attributes`
-    let node = model
-    let selector
-    if (node._elementType === 'element') {
-      selector = {
-        path: [node.id, '_childNodes']
-      }
-    } else {
-      selector = {
-        path: [node.id]
-      }
-    }
-    comp.context.appState.addObserver(['document'], fn, comp, {
-      stage,
-      document: selector
-    })
-  // PropertyModels
-  } else if (model._isValue) {
+  if (model._isValue) {
     let path = model.getPath()
     comp.context.appState.addObserver(['document'], fn, comp, {
       stage,

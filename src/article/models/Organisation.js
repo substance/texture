e92ac43dece4b0e16b907745002eb53799635642
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-02-18 07:32:31 +0100

    Use Organisation.render() for toString()

--- src/article/models/Organisation.js
+++ src/article/models/Organisation.js
@@ -2,12 +2,14 @@ import { DocumentNode, STRING } from 'substance'
 
 export default class Organisation extends DocumentNode {
   toString () {
-    return this.institution
+    return this.render()
   }
 
   render (options = {}) {
     let { institution, division1, division2, division3 } = this
     let result = institution ? [ institution ] : '???'
+    // TODO: do we really want this? Because the divisions might
+    // be necessary to really understand the displayed name
     if (!options.short && institution) {
       if (division1) {
         result.push(', ', division1)

626cc1097b131671a319fdc1acb5e566a39a2bdf
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-02-19 23:56:05 +0100

    Improve test helper.

--- test/shared/testHelpers.js
+++ test/shared/testHelpers.js
@@ -94,6 +94,12 @@ export function doesNotThrowInNodejs (t, fn, descr) {
   if (platform.inNodeJS) {
     t.doesNotThrow(fn, descr)
   } else {
-    fn()
+    let success = false
+    try {
+      fn()
+      success = true
+    } finally {
+      t.ok(success, descr)
+    }
   }
 }

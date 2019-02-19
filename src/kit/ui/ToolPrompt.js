a03aae12a19401166bb81ecb1656aa9cdd206e77
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-02-19 21:47:51 +0100

    ToolPrompt is not a top-level tool-group.

--- src/kit/ui/ToolPrompt.js
+++ src/kit/ui/ToolPrompt.js
@@ -1,6 +1,8 @@
-import ToolPanel from './ToolPanel'
+import ToolGroup from './ToolGroup'
 
-export default class ToolPrompt extends ToolPanel {
+// TODO: explain why this is necessary. Do we really need styles for this?
+// Or could we just use 'group' within Overlay?
+export default class ToolPrompt extends ToolGroup {
   _getClassNames () {
     return 'sc-tool-prompt'
   }

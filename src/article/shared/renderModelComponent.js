6b76f02c467260154fab6b7b77623c764a0b0f84
Daniel <beilinson@gmail.com>
2019-02-20 15:50:22 +0100

    Remove unneded guards.

--- src/article/shared/renderModelComponent.js
+++ src/article/shared/renderModelComponent.js
@@ -4,11 +4,7 @@ export default function renderModelComponent (context, $$, props) {
   const model = props.model
   if (!model) throw new Error("'props.model' is required")
   let ModelComponent = getComponentForModel(context, model)
-  if (ModelComponent) {
-    // LEGACY
-    props.node = model._node
-    return $$(ModelComponent, props)
-  } else {
-    return $$('div')
-  }
+  // LEGACY
+  props.node = model._node
+  return $$(ModelComponent, props)
 }

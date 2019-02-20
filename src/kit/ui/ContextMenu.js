c4fa998a3b4bdd023c21d59e76fb7d5177cea963
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-02-20 10:57:43 +0100

    Don't show the context menu if it has no enabled tools.

--- src/kit/ui/ContextMenu.js
+++ src/kit/ui/ContextMenu.js
@@ -45,18 +45,21 @@ export default class ContextMenu extends ToolPanel {
     Positions the content menu relative to the scrollPane
   */
   _onContextMenuOpened (hints) {
-    let mouseBounds = hints.mouseBounds
-    this.el.removeClass('sm-hidden')
-    let contextMenuWidth = this.el.htmlProp('offsetWidth')
+    // ATTENTION: assuming that the context menu is always only showing enabled tools
+    if (this._derivedState.hasEnabledItem) {
+      let mouseBounds = hints.mouseBounds
+      this.el.removeClass('sm-hidden')
+      let contextMenuWidth = this.el.htmlProp('offsetWidth')
 
-    // By default, context menu are aligned left bottom to the mouse coordinate clicked
-    this.el.css('top', mouseBounds.top)
-    let leftPos = mouseBounds.left
-    // Must not exceed left bound
-    leftPos = Math.max(leftPos, 0)
-    // Must not exceed right bound
-    let maxLeftPos = mouseBounds.left + mouseBounds.right - contextMenuWidth
-    leftPos = Math.min(leftPos, maxLeftPos)
-    this.el.css('left', leftPos)
+      // By default, context menu are aligned left bottom to the mouse coordinate clicked
+      this.el.css('top', mouseBounds.top)
+      let leftPos = mouseBounds.left
+      // Must not exceed left bound
+      leftPos = Math.max(leftPos, 0)
+      // Must not exceed right bound
+      let maxLeftPos = mouseBounds.left + mouseBounds.right - contextMenuWidth
+      leftPos = Math.min(leftPos, maxLeftPos)
+      this.el.css('left', leftPos)
+    }
   }
 }

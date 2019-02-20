023e0124bb5e7d7d1149205233f0806f74463447
Daniel <beilinson@gmail.com>
2019-02-20 09:07:02 +0100

    Keep tool ad separator styles separatly.

--- src/kit/ui/ToolSeparator.js
+++ src/kit/ui/ToolSeparator.js
@@ -3,7 +3,7 @@ import { Component } from 'substance'
 export default class ToolSeparator extends Component {
   render ($$) {
     const label = this.props.label
-    let el = $$('div').addClass('sc-separator')
+    let el = $$('div').addClass('sc-tool-separator')
     if (label) {
       el.append(
         $$('div').addClass('se-label').append(

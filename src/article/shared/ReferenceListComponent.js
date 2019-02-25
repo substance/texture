e5be44ad2048af2daabbef9c5dd9525180dba383
Daniel Beilinson <danielbeilinson@Daniels-Mac-mini.local>
2019-02-25 18:32:04 +0300

    Introduce reference selection.

--- src/article/shared/ReferenceListComponent.js
+++ src/article/shared/ReferenceListComponent.js
@@ -1,13 +1,18 @@
-import { Component } from 'substance'
+import { CustomSurface } from 'substance'
 import { renderNode } from '../../kit'
 import { getPos } from './nodeHelpers'
 
-export default class ReferenceListComponent extends Component {
+export default class ReferenceListComponent extends CustomSurface {
   didMount () {
-    this.context.appState.addObserver(['document'], this.rerender, this, { stage: 'render', document: { path: ['article', 'references'] } })
+    super.didMount()
+
+    const appState = this.context.appState
+    appState.addObserver(['document'], this.rerender, this, { stage: 'render', document: { path: ['article', 'references'] } })
+    appState.addObserver(['selection'], this.rerender, this, { stage: 'render' })
   }
 
   dispose () {
+    super.dispose()
     // TODO: as we have a node for references now, we should turn this into a NodeComponent instead
     this.context.appState.removeObserver(this)
   }
@@ -20,6 +25,7 @@ export default class ReferenceListComponent extends Component {
   }
 
   render ($$) {
+    const sel = this.context.appState.selection
     const bibliography = this._getBibliography()
 
     let el = $$('div').addClass('sc-reference-list')
@@ -31,12 +37,22 @@ export default class ReferenceListComponent extends Component {
     }
 
     bibliography.forEach(ref => {
-      el.append(
-        renderNode($$, this, ref)
-      )
+      const referenceEl = renderNode($$, this, ref)
+        .ref(ref.id)
+        .on('click', this._selectReference.bind(this, ref.id))
+
+      if (sel && sel.customType === 'reference' && sel.data.referenceId === ref.id) {
+        referenceEl.addClass('sm-selected')
+      }
+
+      el.append(referenceEl)
     })
 
     return el
+  }
+
+  _getCustomResourceId () {
+    return 'reference-list'
   }
 
   _getBibliography () {
@@ -45,5 +61,17 @@ export default class ReferenceListComponent extends Component {
       return getPos(a) - getPos(b)
     })
     return references
+  }
+
+  _selectReference (referenceId) {
+    const newSel = {
+      type: 'custom',
+      customType: 'reference',
+      nodeId: referenceId,
+      data: {
+        referenceId
+      }
+    }
+    this.context.editorSession.setSelection(newSel)
   }
 }

8a8f2b0849012ae4eab64e7e87117609de3b23f2
Daniel Beilinson <beilinson@gmail.com>
2019-03-19 17:02:22 +0300

    Reuse abstract dropdown editor.

---
+++ src/article/shared/DropdownEditor.js
@@ -0,0 +1,52 @@
+import {
+  ValueComponent
+} from '../../kit'
+
+export default class DropdownEditor extends ValueComponent {
+  render ($$) {
+    const model = this.props.model
+    const value = model.getValue()
+    let el = $$('div').addClass(this._getClassNames())
+
+    const dropdownSelector = $$('select').ref('input').addClass('se-select')
+      .on('click', this._suppressClickPropagation)
+      .on('change', this._setValue)
+
+    dropdownSelector.append(
+      $$('option').append(this._getLabel())
+    )
+
+    this._getValues().forEach(l => {
+      const option = $$('option').attr({ value: l.id }).append(l.name)
+      if (l.id === value) option.attr({ selected: 'selected' })
+      dropdownSelector.append(option)
+    })
+
+    el.append(dropdownSelector)
+
+    return el
+  }
+
+  _getClassNames () {
+    return 'sc-dropdown-editor'
+  }
+
+  _getLabel () {
+    return this.getLabel('select-value')
+  }
+
+  _getValues () {
+    return []
+  }
+
+  _setValue () {
+    const model = this.props.model
+    const input = this.refs.input
+    const value = input.getValue()
+    model.setValue(value)
+  }
+
+  _suppressClickPropagation (e) {
+    e.stopPropagation()
+  }
+}
--- src/article/shared/LicenseEditor.js
+++ src/article/shared/LicenseEditor.js
@@ -1,41 +1,12 @@
-import {
-  ValueComponent
-} from '../../kit'
+import DropdownEditor from './DropdownEditor'
 import { LICENSES } from '../ArticleConstants'
 
-export default class LicenseEditor extends ValueComponent {
-  render ($$) {
-    const model = this.props.model
-    const value = model.getValue()
-    let el = $$('div').addClass('sc-license-editor')
-
-    const licenseSelector = $$('select').ref('input').addClass('se-select')
-      .on('click', this._suppressClickPropagation)
-      .on('change', this._setLicense)
-
-    licenseSelector.append(
-      $$('option').append(this.getLabel('select-license'))
-    )
-
-    LICENSES.forEach(l => {
-      const option = $$('option').attr({ value: l.id }).append(l.name)
-      if (l.id === value) option.attr({ selected: 'selected' })
-      licenseSelector.append(option)
-    })
-
-    el.append(licenseSelector)
-
-    return el
+export default class LicenseEditor extends DropdownEditor {
+  _getLabel () {
+    return this.getLabel('select-license')
   }
 
-  _setLicense () {
-    const model = this.props.model
-    const input = this.refs.input
-    const value = input.getValue()
-    model.setValue(value)
-  }
-
-  _suppressClickPropagation (e) {
-    e.stopPropagation()
+  _getValues () {
+    return LICENSES
   }
 }

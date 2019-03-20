714f8bdcf7ffbbf0a22769e761b38893249657df
Daniel Beilinson <beilinson@gmail.com>
2019-03-20 15:59:06 +0300

    Add custom abstract renderer.

--- src/article/models/CustomAbstract.js
+++ src/article/models/CustomAbstract.js
@@ -2,7 +2,12 @@ import Abstract from './Abstract'
 import { CONTAINER, STRING, TEXT } from 'substance'
 import { RICH_TEXT_ANNOS } from './modelConstants'
 
-export default class CustomAbstract extends Abstract {}
+export default class CustomAbstract extends Abstract {
+  render (options = {}) {
+    return this.title || ''
+  }
+}
+
 CustomAbstract.schema = {
   type: 'custom-abstract',
   abstractType: STRING,

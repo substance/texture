d5ff896736c5d05fa828b68313af53c1beb1fdee
Daniel Beilinson <beilinson@gmail.com>
2019-03-25 13:40:46 +0300

    Remove redundant schema definition.

--- src/article/models/CustomAbstract.js
+++ src/article/models/CustomAbstract.js
@@ -1,5 +1,5 @@
 import Abstract from './Abstract'
-import { CONTAINER, STRING, TEXT } from 'substance'
+import { STRING, TEXT } from 'substance'
 import { RICH_TEXT_ANNOS } from './modelConstants'
 
 export default class CustomAbstract extends Abstract {
@@ -20,6 +20,5 @@ export default class CustomAbstract extends Abstract {
 CustomAbstract.schema = {
   type: 'custom-abstract',
   abstractType: STRING,
-  title: TEXT(RICH_TEXT_ANNOS),
-  content: CONTAINER('paragraph')
+  title: TEXT(RICH_TEXT_ANNOS)
 }

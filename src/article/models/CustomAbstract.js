ead680609ee0ce3361951a3842e4801f3a64b138
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-03-26 09:01:35 +0100

    Use new enum type for abstractType

--- src/article/models/CustomAbstract.js
+++ src/article/models/CustomAbstract.js
@@ -1,5 +1,5 @@
 import Abstract from './Abstract'
-import { STRING, TEXT } from 'substance'
+import { ENUM, TEXT } from 'substance'
 import { RICH_TEXT_ANNOS } from './modelConstants'
 
 export default class CustomAbstract extends Abstract {
@@ -19,6 +19,6 @@ export default class CustomAbstract extends Abstract {
 
 CustomAbstract.schema = {
   type: 'custom-abstract',
-  abstractType: STRING,
+  abstractType: ENUM(['executive-summary', 'web-summary'], { default: '' }),
   title: TEXT(RICH_TEXT_ANNOS)
 }

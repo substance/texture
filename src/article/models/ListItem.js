b95e4ab5fabfd2508b8c203505620febf5af64fb
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-03-29 21:49:02 +0100

    Generalize support for indent/dedent via TAB

--- src/article/models/ListItem.js
+++ src/article/models/ListItem.js
@@ -1,6 +1,9 @@
 import { DocumentNode, TextNodeMixin, TEXT } from 'substance'
 import { RICH_TEXT_ANNOS, EXTENDED_FORMATTING, LINKS_AND_XREFS, INLINE_NODES } from './modelConstants'
 
+const MIN_LEVEL = 1
+const MAX_LEVEL = 3
+
 export default class ListItem extends TextNodeMixin(DocumentNode) {
   getLevel () {
     return this.level
@@ -13,6 +16,31 @@ export default class ListItem extends TextNodeMixin(DocumentNode) {
 
   getPath () {
     return [this.id, 'content']
+  }
+
+  get canIndent () { return true }
+
+  indent () {
+    let level = this.level
+    if (level < MAX_LEVEL) {
+      this._changeLevel(1)
+    }
+  }
+
+  get canDedent () { return true }
+
+  dedent () {
+    let level = this.level
+    if (level > MIN_LEVEL) {
+      this._changeLevel(-1)
+    }
+  }
+
+  _changeLevel (delta) {
+    this.setLevel(this.level + delta)
+    // HACK: triggering parent explicitly
+    // TODO: find a better solution
+    this.getParent()._itemsChanged()
   }
 
   static isListItem () {
--- src/article/shared/HeadingComponent.js
+++ src/article/shared/HeadingComponent.js
@@ -1,6 +1,17 @@
 import { TextNodeComponent } from '../../kit'
 
 export default class HeadingComponent extends TextNodeComponent {
+  didMount () {
+    this.context.appState.addObserver(['document'], this.rerender, this, {
+      stage: 'render',
+      document: { path: [this.props.node.id] }
+    })
+  }
+
+  dispose () {
+    this.context.appState.removeObserver(this)
+  }
+
   getClassNames () {
     return 'sc-heading sc-text-node'
   }

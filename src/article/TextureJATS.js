041f2698e9b5d528abbb4516ff5341edca0f226e
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-04-12 13:37:01 +0200

    Remove DTD uri from TextureJATS.

--- src/article/TextureJATS.js
+++ src/article/TextureJATS.js
@@ -3,6 +3,6 @@ import TextureJATSData from '../../tmp/TextureJATS.data'
 
 // TODO: it would be great to achieve this via an extension of the JATS schema
 // i.e. instead of compiling a full schema, just compile an increment
-let TextureJATS = XMLSchema.fromJSON(TextureJATSData, 'article', 'TextureArticle 1.0', 'http://substance.io/TextureArticle-1.0.0.dtd')
+let TextureJATS = XMLSchema.fromJSON(TextureJATSData, 'article', 'TextureArticle 1.0', '')
 
 export default TextureJATS

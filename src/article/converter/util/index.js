7a680a5d27edc54c10f1bb556592d8dad12b684f
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-04-12 13:34:55 +0200

    Remove XML schema driven transformations.

--- src/article/converter/util/index.js
+++ src/article/converter/util/index.js
@@ -1,4 +1,2 @@
 export { default as createEmptyJATS } from './createEmptyJATS'
 export * from './domHelpers'
-export { default as pruneText } from './pruneText'
-export { default as validateXML } from './validateXML'

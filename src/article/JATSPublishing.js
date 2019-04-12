db8ee315c164e91649747a9712776cca6e2f0af0
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-04-12 13:36:12 +0200

    Remove compiled JATS schemas.

--- src/article/JATSPublishing.js
+++
@@ -1,9 +0,0 @@
-import { XMLSchema } from 'substance'
-import JATSPublishingData from '../../tmp/JATS-publishing.data'
-
-let JATSPublishing = XMLSchema.fromJSON(JATSPublishingData,
-  'article',
-  '-//NLM//DTD JATS (Z39.96) Journal Publishing DTD v1.1d1 20130915//EN',
-  'JATS-journalpublishing1.dtd')
-
-export default JATSPublishing

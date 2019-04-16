c76517134deb9f5f197d72bdd73e51c46635bff0
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-04-16 11:47:55 +0200

    Export ArticleJATSImporter and Exporter.

--- src/article/converter/jats/index.js
+++ src/article/converter/jats/index.js
@@ -1,3 +1,5 @@
+export { default as ArticleJATSExporter } from './ArticleJATSExporter'
+export { default as ArticleJATSImporter } from './ArticleJATSImporter'
 export { default as jats2internal } from './jats2internal'
 export { default as internal2jats } from './internal2jats'
 export { default as createJatsImporter } from './createJatsImporter'

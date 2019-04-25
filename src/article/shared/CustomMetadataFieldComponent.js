bd84c0bd62346f5a35636119ac1d82ea5927bf3f
Daniel <beilinson@gmail.com>
2019-04-25 10:55:52 +0300

    Use keyword input for keyword editing.

--- src/article/shared/CustomMetadataFieldComponent.js
+++ src/article/shared/CustomMetadataFieldComponent.js
@@ -1,12 +1,26 @@
 import { NodeComponent } from '../../kit'
+import KeywordInput from '../../kit/ui/KeywordInput'
 
 export default class CustomMetadataFieldComponent extends NodeComponent {
   render ($$) {
     let el = $$('div').addClass('sc-custom-metadata-field')
-    el.append(
-      this._renderValue($$, 'name', { placeholder: this.getLabel('enter-custom-field-name') }).addClass('se-field-name'),
-      this._renderValue($$, 'value', { placeholder: this.getLabel('enter-custom-field-value') })
-    )
+    const node = this._getNode()
+
+    if (this.context.editable) {
+      el.append(
+        this._renderValue($$, 'name', { placeholder: this.getLabel('enter-custom-field-name') }).addClass('se-field-name'),
+        $$(KeywordInput, {
+          values: node.values,
+          placeholder: this.getLabel('enter-custom-field-value'),
+          overlayId: node.id
+        }).addClass('se-field-values')
+      )
+    } else {
+      el.append(
+        $$('div').addClass('se-field-name').append(node.name),
+        $$('div').addClass('se-field-values').append(node.values)
+      )
+    }
     return el
   }
 }

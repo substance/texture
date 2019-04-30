f9118e2309c3fb7eb1ff18be077351b7ec9021de
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-04-30 21:54:22 +0200

    Use the unminified substance bundle in web-demo.

--- make.js
+++ make.js
@@ -104,6 +104,7 @@ b.task('build:assets', function () {
   b.copy('./node_modules/inter-ui', DIST + 'lib/inter-ui')
   b.copy('./node_modules/katex/dist', DIST + 'lib/katex')
   b.copy('./node_modules/substance/dist/*.css*', DIST + 'lib/substance/')
+  b.copy('./node_modules/substance/dist/substance.js*', DIST + 'lib/substance/')
   b.copy('./node_modules/substance/dist/substance.min.js*', DIST + 'lib/substance/')
   b.copy('./node_modules/texture-plugin-jats/dist', DIST + 'plugins/texture-plugin-jats')
   b.css('texture.css', DIST + 'texture.css')

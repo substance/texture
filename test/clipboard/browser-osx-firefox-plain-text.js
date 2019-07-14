77b2655f6d4de5a2f9784da5f7b4ab4210a536ad
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-07-15 00:02:37 +0200

    Moved Clipboard and tests back into substance land.

--- test/clipboard/browser-osx-firefox-plain-text.js
+++
@@ -1,2 +0,0 @@
-/* eslint-disable */
-export default "<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\"></head><body>XXX</body></html>\r\n";
--- test/clipboard/browser-osx-firefox-two-paragraphs.js
+++
@@ -1,2 +0,0 @@
-/* eslint-disable */
-export default "<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\"></head><body><p>AAA</p><p>BBB</p></body></html>";

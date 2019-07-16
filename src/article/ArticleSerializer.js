45d31801b60287e486568455e11f7f498c53ecd0
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-07-16 02:12:38 +0200

    Use the original docType to create a corresponding exporter.

--- src/article/ArticleSerializer.js
+++ src/article/ArticleSerializer.js
@@ -1,23 +1,36 @@
 import { prettyPrintXML, platform } from 'substance'
+import { DEFAULT_JATS_SCHEMA_ID } from './ArticleConstants'
 
 export default class ArticleSerializer {
   export (doc, config) {
     let articleConfig = config.getConfiguration('article')
 
-    // TODO: depending on the article type we should use a specifc exporter
-    let exporter = articleConfig.createExporter('jats')
+    // EXPERIMENTAL: I am not sure yet, if this is the right way to use
+    // exportes and transformers.
+    // During import we store the original docType on the document instance
+    // Now we use this to create the corresponding exporter
+    let docType = doc.docType || DEFAULT_JATS_SCHEMA_ID
+
+    let exporter = articleConfig.createExporter(docType, doc)
+    if (!exporter) {
+      console.error(`No exporter registered for "${docType}". Falling back to default JATS importer, but with unpredictable result.`)
+      // Falling back to default importer
+      exporter = articleConfig.createExporter('jats', doc)
+    }
     let res = exporter.export(doc)
     let jats = res.jats
 
-    // TODO: and depending on the article type we should use a specific transformation
-    let transformation = articleConfig.getTransformation('jats')
-    transformation.export(jats)
-
-    // for the purpose of debugging
-    if (platform.inBrowser) {
-      console.info('saving jats', jats.getNativeElement())
+    let transformation = articleConfig.getTransformation(docType)
+    if (transformation) {
+      transformation.export(jats)
     }
 
-    return prettyPrintXML(jats)
+    let xmlStr = prettyPrintXML(jats)
+    // for the purpose of debugging
+    if (platform.inBrowser) {
+      console.info('saving jats', { el: jats.getNativeElement(), xml: xmlStr })
+    }
+
+    return xmlStr
   }
 }

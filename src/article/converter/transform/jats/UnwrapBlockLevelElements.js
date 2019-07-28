d8aa088991b315136ae68e6890e23e9967d49067
Oliver Buchtala <oliver.buchtala@gmail.com>
2019-07-28 12:45:03 +0300

    Fix UnwrapBlockElements transformation.
    Don't prune empty <p> elements that have been there before.

--- src/article/converter/transform/jats/UnwrapBlockLevelElements.js
+++ src/article/converter/transform/jats/UnwrapBlockLevelElements.js
@@ -20,10 +20,12 @@ function _pBlock (p) {
   let parent = p.parentNode
   let children = p.children
   let L = children.length
+  let hasChanged = false
   // doing it reverse so that we don't miss elements due to the ongoing tranformations
   for (var i = L - 1; i >= 0; i--) {
     let child = children[i]
     if (isBlock[child.tagName]) {
+      hasChanged = true
       // create a new <p>
       let newP = parent.createElement('p')
       let childPos = p.getChildIndex(child)
@@ -32,23 +34,22 @@ function _pBlock (p) {
       // and insert the block element and the new one after the current <p>
       let pos = parent.getChildIndex(p) + 1
       parent.insertAt(pos, child)
-      if (siblings.length > 0 && _needsP(siblings)) {
+      if (siblings.length > 0 && !_isEmpty(siblings)) {
         newP.append(siblings)
         parent.insertAt(pos + 1, newP)
       }
     }
   }
   // if the original <p> is now empty, remove it
-  if (!_needsP(p.childNodes)) {
+  if (hasChanged && _isEmpty(p.childNodes)) {
     p.parentNode.removeChild(p)
   }
 }
 
-function _needsP (nodes) {
+function _isEmpty (nodes) {
   for (let i = 0; i < nodes.length; i++) {
     let child = nodes[i]
-    // don't prune if there is something else left
-    if (!child.isTextNode() || !(/^\s*$/.exec(child.textContent))) return true
+    if (!child.isTextNode() || !(/^\s*$/.exec(child.textContent))) return false
   }
-  return false
+  return true
 }
